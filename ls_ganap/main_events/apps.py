8b574086b065903c12c297c6f4cbad3873621a36
Daniel <daniel.delrio@obf.ateneo.edu>
2018-06-11 14:20:35 +0800

    Made the main_events app and added the models

---
+++ ls_ganap/main_events/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainEventsConfig(AppConfig):
+    name = 'main_events'
---
+++ ls_ganap/main_events/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class EventHost(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.TextField()
+	color = models.CharField(max_length=20)
+	logo_url = models.URLField()
+
+class Venue(models.Model):
+	name = models.CharField(max_length=200)
+
+class Event(models.Model):
+	venue_id = models.ForeignKey(Venue, on_delete=models.SET_NULL)
+	host_id = models.ForeignKey(EventHost, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DataTimeField(auto_now_add=True)
+    updated_at = models.DataTimeField(auto_now=True)
+    is_accepted = models.BooleanField(default=False)
+    poster_url = models.URLField()
+    outside_venue_name = models.CharField(max_length=200, blank=True)
+    is_premium = models.BooleanField(default=False)
+    event_url = models.URLField()
+        
+class Tag(models.Model):
+	name = models.CharField(max_length=50)
+
+class TagToEvent(models.Model):
+	tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+	event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+	
+class TagSubscription(models.Model):
+	# user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+	
+class EventHostSubscription(models.Model):
+	# user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	event_host_id = models.ForeignKey(EventHost, on_delete=models.CASCADE)
