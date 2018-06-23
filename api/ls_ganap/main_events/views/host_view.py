cabd2702fc922e284bb7b3eb10ce5bc5b3d62990
Daniel <daniel.delrio@obf.ateneo.edu>
2018-06-23 15:15:21 +0800

    Implement RetrieveUpdateDestroyAPIView for EventHost and Tag

--- api/ls_ganap/main_events/views/host_view.py
+++ api/ls_ganap/main_events/views/host_view.py
@@ -20,27 +20,6 @@ class HostList(generics.ListCreateAPIView):
         serializer = HostSerializer(queryset, many=True)
         return Response(serializer.data)
 
-class HostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return EventHost.objects.get(pk=pk)
-        except EventHost.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        host = self.get_object(pk)
-        serializer = HostSerializer(host)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        host = self.get_object(pk)
-        serializer = HostSerializer(host, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        host = self.get_object(pk)
-        host.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class HostDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = EventHost.objects.all()
+	serializer_class = HostSerializer
