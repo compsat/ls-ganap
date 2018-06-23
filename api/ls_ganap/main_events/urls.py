e2927c13c583ab0a2cbf8eb4d4924a88d7253625
Daniel <daniel.delrio@obf.ateneo.edu>
2018-06-23 16:50:38 +0800

    Add venues/ route

--- api/ls_ganap/main_events/urls.py
+++ api/ls_ganap/main_events/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from main_events.views import event_view, host_view, tag_view
+from main_events.views import event_view, host_view, tag_view, venue_view
 # import views
 
 urlpatterns = [
@@ -11,6 +11,8 @@ urlpatterns = [
     path('event_hosts/<int:pk>/', host_view.HostDetail.as_view()),
     path('tags/', tag_view.TagList.as_view()),
     path('tags/<int:pk>/', tag_view.TagDetail.as_view()),
+    path('venues/', venue_view.VenueList.as_view()),
+    path('venues/<int:pk>/', venue_view.VenueDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
