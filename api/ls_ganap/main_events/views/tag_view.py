42d027d26db29c7a245ca7964053307ec9d20b70
Daniel <daniel.delrio@obf.ateneo.edu>
2018-06-23 15:15:21 +0800

    Implement RetrieveUpdateDestroyAPIView for EventHost and Tag

--- api/ls_ganap/main_events/views/tag_view.py
+++ api/ls_ganap/main_events/views/tag_view.py
@@ -20,27 +20,6 @@ class TagList(generics.ListCreateAPIView):
         serializer = TagSerializer(queryset, many=True)
         return Response(serializer.data)
         
-class TagDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Tag.objects.get(pk=pk)
-        except Tag.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        tag = self.get_object(pk)
-        serializer = TagSerializer(tag)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        tag = self.get_object(pk)
-        serializer = TagSerializer(tag, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        tag = self.get_object(pk)
-        tag.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Tag.objects.all()
+	serializer_class = TagSerializer
