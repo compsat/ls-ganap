ac88ae3ac1d818527c228e433d850d714ee14bfa
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-06-26 15:16:56 +0800

    Overwrite get_queryset() method to search events by name

--- api/ls_ganap/main_events/views/event_view.py
+++ api/ls_ganap/main_events/views/event_view.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 # import the pagination settings
 from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumberPagination
 from rest_framework import status
+# import Query lookups
+from django.db.models import Q
 
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
@@ -14,10 +16,28 @@ class EventList(generics.ListCreateAPIView):
     # specifies which pagination settings to follow
     pagination_class = ObjectPageNumberPagination
 
+    # overwrite get_queryset() method
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Event.objects.all()
+        query = self.request.GET.get("q")
+        
+        # only perform the filtering if the query has arguements
+        # if not return all the events
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(name__icontains=query)
+            ).distinct()
+
+        return queryset_list
+
+
     def list_items(self, request):
-        queryset = self.get_queryset()
+        # make sure to filter by event start_time
+        queryset = self.get_queryset().order_by('start_time')
+        
         serializer = EventSerializer(queryset, many=True)
         return Response(serializer.data)
+
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Event.objects.all()
