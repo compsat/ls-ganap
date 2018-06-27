82a80a86234e2fdf4507f9742469b5adb80ce4ac
Nikki Domingo <nikkidomingo@Nikkis-MacBook-Pro.local>
2018-06-27 15:35:33 +0700

    Add navbar structure

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import logo from 'assets/img/logo.png';
+import '../Nav.css'
 
 class MainNav extends Component {
   render() {
     return (
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/browse">Browse</Link>
-          </li>
-        </ul>
+        <div className='mobile-nav'>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <div className='logo'>Logo</div>
+              {/* <img className='logo' src={logo} alt='LS Ganap Logo'/> */}
+            </li>
+            <li>
+              <Link to="/browse">Browse</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='desktop-nav'>
+          <ul>
+            <li>
+              <div className='logo'>Logo</div>
+              {/* <img className='logo' src={logo} alt='LS Ganap Logo'/> */}
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/browse">Browse</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
