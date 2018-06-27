2c11d8ec653d668b3bcf876fe22ab6a600b03a3d
Nikki Domingo <nikkidomingo@Nikkis-MacBook-Pro.local>
2018-06-27 15:50:47 +0700

    Add basic navbar styles

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import logo from 'assets/img/logo.png';
+// import logo from '../assets/ls-ganap-logo.svg';
 import '../Nav.css'
 
 class MainNav extends Component {
