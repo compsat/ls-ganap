4e0af543956ef02319820ffa379e91a299ce6f49
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-02 23:01:24 +0800

    Add Filter events by sepcific date and filter events between dates

--- api/ls_ganap/main_events/views/event_view.py
+++ api/ls_ganap/main_events/views/event_view.py
@@ -9,6 +9,43 @@ from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumber
 from rest_framework import status
 # import Query lookups
 from django.db.models import Q
+from django.http import Http404
+from datetime import datetime, timedelta
+
+
+class FilterEventsBetweenDates(generics.ListAPIView):
+    serializer_class = EventSerializer
+    """
+    sample usage http://localhost:8000/api/get_events_between/?start_date=2018-06-25&end_date=2018-07-03
+    Note: range does not include the last element need to add 
+    """
+    def get_queryset(self):
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+        end_date = datetime.strptime(end_date, '%Y-%m-%d') + timedelta(hours=23, minutes=59, seconds=59, milliseconds=59)
+
+        queryset = Event.objects.all()
+
+        if(start_date is not None) and (end_date is not None):
+           queryset = queryset.filter(start_time__range=[start_date, end_date])
+        else:
+            raise Http404
+
+        return queryset
+
+class FilterEventByDate(generics.ListAPIView):
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        date = self.kwargs['date']
+        queryset = Event.objects.all()
+        
+        if date is not None:
+            queryset = queryset.filter(start_time__date=date)
+
+        return queryset
+
+
 
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
