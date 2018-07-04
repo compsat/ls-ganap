9855b502a0f14193d85214f73b899561a18ec51b
Daniel <daniel.delrio@obf.ateneo.edu>
2018-07-04 19:12:34 +0800

    Change type in HostType to type_name

--- api/ls_ganap/main_events/models.py
+++ api/ls_ganap/main_events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class HostType(models.Model):
-	type = models.CharField(max_length=20)
+	type_name = models.CharField(max_length=20)
 
 class EventHost(models.Model):
 	name = models.CharField(max_length=200)
