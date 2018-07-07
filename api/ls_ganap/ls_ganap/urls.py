f9d84f5cd8ab1e7949bd9eaf6960e54adc73d1f1
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-07 16:18:24 +0800

    Change default route to admin

--- api/ls_ganap/ls_ganap/urls.py
+++ api/ls_ganap/ls_ganap/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('main_events.urls')),
 ]
