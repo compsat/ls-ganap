564df31393afe5867730c3d18144168d269d1d8f
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-07 14:33:09 +0800

    Remove filterbackends

--- api/ls_ganap/ls_ganap/settings.py
+++ api/ls_ganap/ls_ganap/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend'   
 
 }   
 
