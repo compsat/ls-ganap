4bc10e7102b3255eb243c36227ae90efe37ee6d8
Miguel N. Galace <galacemiguel@gmail.com>
2018-07-07 18:00:55 +0800

    Remove React wrapper components

--- client/src/components/FullWidthContainer.js
+++ client/src/components/FullWidthContainer.js
@@ -1,19 +1,8 @@
-import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const FullWidthContainer = styled.div`
   margin-left: calc((100% - 100vw)/2);
   margin-right: calc((100% - 100vw)/2);
 `
-
-class FullWidthContainer extends Component {
-  render() {
-    return (
-      <Container>
-        {this.props.children}
-      </Container>
-    );
-  }
-}
 
 export default FullWidthContainer;
