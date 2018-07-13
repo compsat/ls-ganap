65c634b46c0ecea75e7601e32cb4d41116830bc8
Nikki Domingo <nikkiii.domingo@gmail.com>
2018-07-13 09:58:34 +0800

    Add changes to MainNav.js

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,41 +1,106 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import logo from '../assets/ls-ganap-logo.svg';
-import '../Nav.css'
+import logo from '../assets/ls-ganap-logo.png';
+import styled from 'styled-components';
+import FullWidthContainer from './FullWidthContainer';
+import PageContent from './PageContent';
+
+const NavLink = ({className, route, children}) => (
+    <Link className={className} to={route}>{children}</Link>
+  )
+  
+  const Image = ({className, source, alt}) => (
+    <img className={className} src={source} alt={alt}/>
+  )
+  
+  const Nav = FullWidthContainer.extend`
+    width: 100%;
+    height: 100px;
+  
+    font-family: sans-serif;
+    text-transform: uppercase;
+  
+    @media all and (min-width: 480px) {
+      border-top: solid 10px #C5A478;
+  }
+`;
+
+const NavList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 100px;
+  margin: 0; 
+  padding: 0;
+
+  @media all and (min-width: 480px) {
+    grid-template-columns: 1fr 150px 150px;
+    width: 100%
+  }
+`;
+
+const NavListItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+
+  @media all and (min-width: 480px) {
+    justify-content: flex-start;
+  }
+`;
+
+const Logo = styled(Image)`
+  max-height: 50px;
+`;
+
+const MobileLink = styled(NavLink)`
+  display: block;
+  color: #E07B24;
+  text-decoration: none;
+
+  @media all and (min-width: 480px) {
+    display: none;
+  }
+`;
+
+const DesktopLink = styled(NavLink)`
+  display: none;
+  color: #E07B24;
+  text-decoration: none;
+
+  @media all and (min-width: 480px) {
+    display: block;
+  }
+`;
+
+const DesktopLogo = DesktopLink.extend`
+  @media all and (min-width: 480px) {
+    display:block;
+    text-align: right;
+  }
+`;
 
 class MainNav extends Component {
   render() {
     return (
-      <nav>
-        <div className='mobile-nav'>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <div className='logo'>Logo</div>
-              {/* <img className='logo' src={logo} alt='LS Ganap Logo'/> */}
-            </li>
-            <li>
-              <Link to="/browse">Browse</Link>
-            </li>
-          </ul>
-        </div>
-        <div className='desktop-nav'>
-          <ul>
-            <li>
-              <div className='logo'>Logo</div>
-              {/* <img className='logo' src={logo} alt='LS Ganap Logo'/> */}
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/browse">Browse</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <Nav>
+      <PageContent>
+        <NavList>
+          <NavListItem>
+            <MobileLink route="/">Home</MobileLink>
+            <DesktopLogo route="/"><Logo source={logo} alt='LS Ganap Logo' /></DesktopLogo>
+          </NavListItem>
+          <NavListItem>
+            <MobileLink route="/"><Logo source={logo} alt='LS Ganap Logo' /></MobileLink>
+            <DesktopLink route="/">Home</DesktopLink>
+          </NavListItem>
+          <NavListItem>
+            <MobileLink route="/">Browse</MobileLink>
+            <DesktopLink route="/browse">Browse</DesktopLink>
+          </NavListItem>
+        </NavList>
+      </PageContent>
+    </Nav>
     );
   }
 }
