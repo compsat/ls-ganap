3d213ce09479639b07b5be0e747e5d5e965e5e7c
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-16 23:39:10 +0800

    Make scraper for coa website

--- api/ls_ganap/main_events/fixtures/scrape_orgs.py
+++ api/ls_ganap/main_events/fixtures/scrape_orgs.py
@@ -32,36 +32,68 @@ def log_error(e):
 
 
 raw_html = simple_get("https://web.archive.org/web/20171029050715/http://coacentral.org:80/organizations/")
-# raw_html = simple_get("http://google.com")
-# print(raw_html)
 
 html = bs(raw_html, 'html.parser')
+
+file = open('organizations.yaml', 'w')
+
 for i, org in enumerate(html.findAll("div", {"class": "org-thumbnail"})):
 	
 	if org.p is not None:
-
+		print(i, org.p.text.strip())
+		file.write('- model: main_events.eventhost\n')
 		#Org name
-		print(i, org.p.text)	
+		file.write('  pk: {}\n'.format(i+1))
+		file.write('  fields:\n')
+		file.write('    host_type: 1\n')
+		
+		#Name
+		# print(i, org.p.text.strip())
+		file.write('    name: {}\n'.format(org.p.text.strip()))
+
 		org_link = org.p['onclick'].split("=")[1][1:-2]
 
 		#Abreviation
-		print(org_link.split('organizations/')[1])
+		try: 
+			# print(org_link.split('organizations/')[1])
+			file.write('    abreviation: {}\n'.format(org_link.split('organizations/')[1]))	
+		except IndexError:
+			# print('    abreviation: FIX')
+			file.write('    abreviation: FIX\n')
 
 		org_page_raw = simple_get(org_link)
 
 		org_page_data = bs(org_page_raw, 'html.parser')
-		cluster = org_page_data.select('#page-subtitle')[0].text
-		description = org_page_data.select('#org-description-description')[0].p.text
-		
+
+
 		#Cluster
-		print(cluster)
+		try:
+			cluster = org_page_data.select('#page-subtitle')[0].text
+			# print(cluster)
+			file.write('    cluster: {}\n'.format(cluster))
+
+		except IndexError:
+			# print('    cluster: FIX')
+			file.write('    cluster: FIX\n')
+			
 
 		#Description
-		print(description)
+		try:
+			# description = org_page_data.select('#org-description-description')[0].p.text.strip()
+			description = org_page_data.select('#org-description-description')
+
+			for x in description:
+				paragraphs = x.findAll('p')
+				result = "";
+				for para in paragraphs:
+					result += para.text.strip()
+			# print(result)	
+			file.write('    description: {}\n'.format(" ".join(result.split('\n'))))
+
+		except IndexError:
+			# print('    desciption: FIX')
+			file.write('    description: FIX\n')
+
 		
-
-
-
-
 
 	
