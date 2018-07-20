8bbb31a854a6145b06f142d3cf052efff94060d2
Daniel <daniel.delrio@obf.ateneo.edu>
2018-07-20 22:51:40 +0800

    Remove Detail View for Tag and TagEventsList

--- api/ls_ganap/main_events/views/tag_view.py
+++ api/ls_ganap/main_events/views/tag_view.py
@@ -1,5 +1,5 @@
-from main_events.models import Tag, Event
-from main_events.serializers import TagSerializer, EventSerializer
+from main_events.models import Tag
+from main_events.serializers import TagSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import generics
@@ -23,38 +23,3 @@ class TagList(generics.ListCreateAPIView):
         queryset = self.get_queryset()
         serializer = TagSerializer(queryset, many=True)
         return Response(serializer.data)
-        
-class TagDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    get: 
-    Returns a tag given its id
-    
-    put:
-    Updates a tag given its id
-
-    patch:
-    Updates a tag given its id
-
-    delete:
-    Deletes a tag given its id
-    """
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-
-class TagEventsList(generics.ListAPIView):
-    """
-    get: List all the events with a tag given its id.
-    """
-    serializer_class = EventSerializer
-    pagination_class = ObjectPageNumberPagination
-
-    def get_queryset(self):
-        tag_id = self.kwargs['pk']
-        queryset = Event.objects.all()
-        
-        if Tag.objects.filter(pk=tag_id).exists():
-            queryset = queryset.filter(tags=tag_id)
-        else:
-            raise Http404
-
-        return queryset
