98c5e6e855167dc5ec6455b868bd52b16c242776
Nikki Domingo <nikkiii.domingo@gmail.com>
2018-07-20 11:23:17 +0800

    Style Featured Section for desktop

--- client/src/home/FeaturedSection.js
+++ client/src/home/FeaturedSection.js
@@ -17,6 +17,7 @@ const FeaturedContainer = styled.div`
         display:grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: 1fr;
+        grid-column-gap: 1em;
   `}
 `;
 
@@ -46,26 +47,94 @@ const LinksSection = styled.div`
     text-align: right;
 `;
 
+const LinkContainer = styled.div`
+    width: 100%;
+    font-size: 0.7em;
+
+    ${media.mdScreen`
+        margin-top: 2.5em;
+    `}
+`;
+
 const Link = ({className, route, children}) => (
-    <a className={className} href={route}>{children}</a>
+    <LinkContainer>
+        <a className={className} href={route}>{children}</a>
+    </LinkContainer>
 );
 
-const StyledLink = styled(Link)`
+
+const MobileLink = styled(Link)`
     width: 100%;
     color: #926C00;
     text-decoration: none;
 `;
 
-const DatesContainer = styled.div`
-    font-size: 1em;
+const DesktopLink = styled(Link)`
+    background-color: #926C00;
+    color: white;
+    
+    font-family: sans-serif;
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 1em 2em;
 `;
 
-const DetailsContainer = styled.div`
+const DesktopLink2 = DesktopLink.extend`
+    background-color: white;
+    color: #926C00;
 `;
+
+const DatesContainer = styled.div`
+    font-size: 1em;
+
+    ${media.mdScreen`
+        display:grid;
+        grid-template-columns: 1fr 2fr;
+        grid-template-rows: 1fr 1fr 1fr;
+    `}
+`;
+
+const DesktopDetailsContainer = styled.div`
+    display: none;
+
+    ${media.mdScreen`
+        display: grid;
+        grid-template-rows: 1fr 1.5fr 1fr;
+        height: 100%;
+    `}
+`;
+
+const MobileDetailsContainer = styled.div`
+    display: block;
+
+    ${media.mdScreen`
+        display: none;
+    `}
+`;
+
+const DetailsContainer = styled.div``;
+
+const Heading = styled.div``;
+const Content = styled.div``;
 
 const DesktopDetails = () => (
     <DesktopDetailsContainer>
-        Desktop details
+        <TitleSection>
+            <Title>Rent</Title>
+            <Subtitle>by blueREP</Subtitle>
+        </TitleSection>
+        <DatesContainer>
+            <Heading>Date/Time:</Heading>
+            <Content>April 7, 2018 (02:00PM - 03:00PM)</Content>
+            <Heading>Venue:</Heading>
+            <Content>Rizal Mini Theatre, Ateneo de Manila University</Content>
+            <Heading>Ticket Prices:</Heading>
+            <Content>AP400 (Regular price) <br />P350 (Student discount)</Content>
+        </DatesContainer>
+        <LinksSection>
+            <DesktopLink route="">Export to Calendar</DesktopLink>
+            <DesktopLink2 route="">Read More about Rent</DesktopLink2>
+        </LinksSection>
     </DesktopDetailsContainer>
 );
 
@@ -77,8 +146,8 @@ const MobileDetails = () => (
                 <Subtitle>by blueREP</Subtitle>
             </TitleSection>
             <LinksSection>
-                <StyledLink route="">Export</StyledLink><br />
-                <StyledLink route="">Read More</StyledLink>
+                <MobileLink route="">Export</MobileLink><br />
+                <MobileLink route="">Read More</MobileLink>
             </LinksSection>
         </TitleContainer>
         <DatesContainer>
@@ -86,22 +155,6 @@ const MobileDetails = () => (
         </DatesContainer>
     </MobileDetailsContainer>
 );
-
-const DesktopDetailsContainer = styled.div`
-    display: none;
-
-    ${media.mdScreen`
-        display: block;
-    `}
-`;
-
-const MobileDetailsContainer = styled.div`
-    display: block;
-
-    ${media.mdScreen`
-        display: none;
-    `}
-`;
 
 class FeaturedSection extends Component {
 
