0c08fe49551de04c68aae1bcd57685f904474444
Daniel <daniel.delrio@obf.ateneo.edu>
2018-07-20 22:51:12 +0800

    Add TypeHostList and ClusterOrgsList to host_view

--- api/ls_ganap/main_events/views/host_view.py
+++ api/ls_ganap/main_events/views/host_view.py
@@ -1,5 +1,5 @@
-from main_events.models import EventHost, Event
-from main_events.serializers import HostSerializer, EventSerializer
+from main_events.models import EventHost, Cluster, HostType
+from main_events.serializers import HostSerializer, HostTypeDetailSerializer, ClusterDetailSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import generics
@@ -9,10 +9,9 @@ from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumber
 from rest_framework import status
 
 
-class HostList(generics.ListCreateAPIView):
+class HostList(generics.ListAPIView):
     """
     get: List all the hosts.
-    post: Create a new host.
     """
     queryset = EventHost.objects.all()
     serializer_class = HostSerializer
@@ -38,20 +37,18 @@ class HostDetail(generics.RetrieveUpdateAPIView):
     queryset = EventHost.objects.all()
     serializer_class = HostSerializer
 
-class HostEventsList(generics.ListAPIView):
+class TypeHostList(generics.RetrieveAPIView):
     """
-    get: List all the events of a host given its id.
+    get: Returns a host type given its id along with the hosts under it.
     """
-    serializer_class = EventSerializer
+    queryset = HostType.objects.all()
+    serializer_class = HostTypeDetailSerializer
     pagination_class = ObjectPageNumberPagination
 
-    def get_queryset(self):
-        host_id = self.kwargs['pk']
-        queryset = Event.objects.all()
-        
-        if EventHost.objects.filter(pk=host_id).exists():
-            queryset = queryset.filter(host_id=host_id)
-        else:
-            raise Http404
-
-        return queryset
+class ClusterOrgsList(generics.RetrieveAPIView):
+    """
+    get: Returns a cluster given its id along with the orgs under it.
+    """
+    queryset = Cluster.objects.all()
+    serializer_class = ClusterDetailSerializer
+    pagination_class = ObjectPageNumberPagination
