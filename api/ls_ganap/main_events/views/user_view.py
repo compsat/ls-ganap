4229f266b813b058c8fc1abbbf18f48b1b0fcde3
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-22 22:33:03 +0800

    Create users views - list and detail

---
+++ api/ls_ganap/main_events/views/user_view.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from main_events.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+# import the pagination settings
+from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumberPagination
+from rest_framework import status
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
