db45d2c69e8a29b9ff0df6742a53f9b050c42822
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-28 17:28:44 +0800

    Create UserCreate and UserLogin serializer

--- api/ls_ganap/main_events/serializers/user_serializer.py
+++ api/ls_ganap/main_events/serializers/user_serializer.py
@@ -1,19 +1,40 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from django.db.models import Q
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 class UserCreateSerializer(serializers.ModelSerializer):
+    confirm_email = serializers.EmailField(label='Confirm email')
     class Meta:
         model = User
 
         fields = ['email', 
+                  'confirm_email',
         		  'password']
-        		  
-        # extra_kwargs = {'password': 
-        # 					{'write-only': True}
-    				# 	}
+        
+        # doesn't show the password on the request	  
+        extra_kwargs = {'password': 
+        					{'write_only': True}
+    					}
+
+
+    # overwrite the validated_data
+    def create(self, validated_data):
+
+        email = validated_data['email']
+        password = validated_data['password']
+
+        user_obj = User(
+                email=email,
+            )
+
+        # this is where we actually set the password
+        user_obj.set_password(password)
+        user_obj.save()
+
+        return validated_data
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,3 +42,51 @@ class UserSerializer(serializers.ModelSerializer):
 
         fields = ['email', 
         		  'password']
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(allow_blank=True, read_only=True)
+    email = serializers.EmailField(label='Email Address')
+    class Meta:
+        model = User
+
+        fields = ['email', 
+                  'password',
+                  'token']
+        
+        # doesn't show the password on the request    
+        extra_kwargs = {'password': 
+                            {'write_only': True}
+                        }
+
+
+    def validate(self, data):
+        
+        user_obj = None
+        email = data['email']
+        password = data['password']
+
+        if not email:
+            raise Validation('An email must exist.')
+
+        # making sure that this email exists
+        user = User.objects.filter(
+                Q(email=email)
+            ).distinct()
+
+
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError('This username is not valid.')
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError('Incorrect credentials please try again.')
+            
+        data['token'] = 'SOME RANDOM TOKEN'
+
+
+        return data
+
+
