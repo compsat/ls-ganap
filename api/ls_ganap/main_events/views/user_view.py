3a003cb3d533b68ab3cbb2d2681c354f9896ef6d
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-07-28 17:29:50 +0800

    Modify permissions in event view and add user views

--- api/ls_ganap/main_events/views/user_view.py
+++ api/ls_ganap/main_events/views/user_view.py
@@ -5,12 +5,14 @@ from rest_framework import generics
 from rest_framework.response import Response
 # import the pagination settings
 from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumberPagination
-from rest_framework import status
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.permissions import AllowAny
+from main_events.serializers import user_serializer
 
 
 User = get_user_model()
 
-class UserList(generics.CreateAPIView):
+class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = user_serializer.UserCreateSerializer
 
@@ -18,3 +20,16 @@ class UserList(generics.CreateAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = user_serializer.UserSerializer
+
+
+class UserLoginAPIView(APIView):
+	permission_classes = [AllowAny]
+	serializer_class = user_serializer.UserLoginSerializer
+
+	def post(self, request, *args, **kwargs):
+		data = request.data
+		serializer = user_serializer.UserLoginSerializer(data=data)
+		if serializer.is_valid(raise_exception=True):
+			new_data = serializer.data
+			return Response(new_data, status=HTTP_200_OK)
+		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
