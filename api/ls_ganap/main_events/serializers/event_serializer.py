734c8a183638868c503d1605f9cc2a0baec29e5a
Daniel <daniel.delrio@obf.ateneo.edu>
2018-08-01 00:02:41 +0800

    Serialize new fields in Event

--- api/ls_ganap/main_events/serializers/event_serializer.py
+++ api/ls_ganap/main_events/serializers/event_serializer.py
@@ -1,18 +1,26 @@
-from rest_framework import serializers
-from main_events.models import Event, Tag
+from rest_framework import serializers, fields
+from main_events.models import Event, Tag, WEEKDAYS
+#from main_events.serializers.recurrence_serializer import RuleSerializer
 
 class EventSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
+    # recurrence = RuleSerializer
+    recur_days = fields.MultipleChoiceField(choices=WEEKDAYS)
 
     class Meta:
         model = Event
         fields = ['id', 
+                'host', 
+                'name', 
+                'description',
+                'venue',
                 'start_time', 
                 'end_time',
-                'venue', 
-                'host',
-                'name', 
-                'description', 
+                'freq',
+                'repeats',
+                'recur_days',
+                'end_recur_date',
+                'end_recur_times',
                 'is_accepted', 
                 'poster_url', 
                 'outside_venue_name', 
