1fe177ef2b5903c3d984b47c680b4a1faea6948e
Daniel <daniel.delrio@obf.ateneo.edu>
2018-08-02 00:37:13 +0800

    Add serializer for Recurrence endpoint

--- api/ls_ganap/main_events/serializers/event_serializer.py
+++ api/ls_ganap/main_events/serializers/event_serializer.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers, fields
-from main_events.models import Event, Tag, WEEKDAYS
-#from main_events.serializers.recurrence_serializer import RuleSerializer
+from main_events.models import Event, Tag, Recurrence, WEEKDAYS
 
 class EventSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
-    # recurrence = RuleSerializer
-    recur_days = fields.MultipleChoiceField(choices=WEEKDAYS)
 
     class Meta:
         model = Event
@@ -16,14 +13,47 @@ class EventSerializer(serializers.ModelSerializer):
                 'venue',
                 'start_time', 
                 'end_time',
-                'freq',
-                'repeats',
-                'recur_days',
-                'end_recur_date',
-                'end_recur_times',
+                'recurrence_bool',
                 'is_accepted', 
                 'poster_url', 
                 'outside_venue_name', 
                 'is_premium', 
                 'event_url',
                 'tags']
+
+class EventDetailSerializer(serializers.ModelSerializer):
+    datetimes = serializers.ListField(child=serializers.DateTimeField())
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
+
+    class Meta:
+        model = Event
+        fields = ['id', 
+                'host', 
+                'name', 
+                'description',
+                'venue',
+                'start_time', 
+                'end_time',
+                'recurrence_bool',
+                'datetimes',
+                'is_accepted', 
+                'poster_url', 
+                'outside_venue_name', 
+                'is_premium', 
+                'event_url',
+                'tags']
+
+class RecurrenceSerializer(serializers.ModelSerializer):
+    recur_days = fields.MultipleChoiceField(choices=WEEKDAYS)
+    # datetimes = serializers.ListField(child=serializers.DateTimeField())
+    event = EventDetailSerializer(required=True)
+
+    class Meta:
+        model = Recurrence
+        fields = ['event',
+                'freq',
+                'repeats',
+                'recur_days',
+                'end_recur_date',
+                'end_recur_times',
+                'datetimes']
