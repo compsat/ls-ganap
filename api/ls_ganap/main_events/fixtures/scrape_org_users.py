be165ec7d43051f2f17cbceab2f0c2451367793b
Miguel Habana <mighabana@gmail.com>
2018-08-07 16:45:09 +0800

    Fix scraper for linking EventHost to a User

--- api/ls_ganap/main_events/fixtures/scrape_org_users.py
+++ api/ls_ganap/main_events/fixtures/scrape_org_users.py
@@ -5,18 +5,33 @@ data1 = pd.read_csv('organizations.csv')
 file = open('org_users.yaml', 'w')
 
 for index, rows in data.iterrows():
-    file.write('- model: main_events.orguser\n')
+    file.write('- model: main_events.user\n')
     file.write('  pk: ' +str(index +1)+'\n')
     file.write('  fields:\n')
     file.write('    email: ' + rows[2] + rows[3][1:] + '\n')
     file.write('    password: ' + rows[2] + '\n')
-    
-    eh_id = "#FIX"
-
-    for i, r in data1.iterrows():
-        if(rows[1].lower() == r[4].lower()):
-            eh_id = r[1]
-
-    file.write('    event_host: ' + str(eh_id) + '\n')
 
 file.close()
+
+file = open('organizations.yaml', 'w')
+
+for index, rows in data1.iterrows():
+    file.write('- model: main_events.orghost\n')
+    file.write('  pk: ' + str(rows[1]) + '\n')
+    file.write('  fields:\n')
+
+    user_id = "#FIX"
+
+    for i, r in data.iterrows():
+        if (rows[4].lower() == r[1].lower()):
+            user_id = i + 1
+
+    # file.write('    user_id: ' + str(user_id) + '\n')
+    file.write('    name: ' + rows[3] + '\n')
+    file.write('    abbreviation: ' + rows[4] + '\n')
+    file.write('    description: ' + rows[8] + '\n')
+    file.write('    color: \'' + rows[6] + '\'\n')
+    file.write('    logo_url: ../' + rows[4] + '\n')
+    file.write('    event_host: 1')
+    file.write('    org_type: 1')
+    file.write('    cluster: ' + str(rows[5]) + '\n')
