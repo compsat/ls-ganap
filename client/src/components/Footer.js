24be4a08bec2c6e819af370a05294f1db52fa11a
Miguel N. Galace <galacemiguel@gmail.com>
2018-08-12 20:40:59 +0800

    Add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        
+      </footer>
+    );
+  }
+}
+
+export default Footer;
