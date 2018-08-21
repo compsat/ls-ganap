7e7b2aa47eb737bb869544e927d81de416714d76
Miguel Habana <mighabana@gmail.com>
2018-08-21 18:48:48 +0800

    Update models.py to include a user field for OrgHosts

--- api/ls_ganap/main_events/models.py
+++ api/ls_ganap/main_events/models.py
@@ -52,7 +52,7 @@ class User(AbstractUser):
     objects = UserManager()
 
 class OrgType(models.Model):
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=50)
 	abbreviation = models.CharField(max_length=3)
 
 	def __str__(self):
@@ -73,7 +73,6 @@ class Cluster(models.Model):
 		ordering = ('name',)
 
 class EventHost(models.Model):
-	# user_id = models.OneToOneField(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200)
 
 	def __str__(self):
@@ -111,6 +110,7 @@ class OfficeHost(models.Model):
 		ordering = ('name',)
 
 class OrgHost(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200)
 	abbreviation = models.CharField(max_length=10, blank=True)
 	description = models.TextField()
