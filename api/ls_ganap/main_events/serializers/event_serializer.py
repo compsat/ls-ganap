f826142f2e63f5794ca7b90fdbc8b28367a3f7ca
Daniel <daniel.delrio@obf.ateneo.edu>
2018-08-21 13:43:40 +0800

    Add hosts to EventSerializer

--- api/ls_ganap/main_events/serializers/event_serializer.py
+++ api/ls_ganap/main_events/serializers/event_serializer.py
@@ -21,15 +21,15 @@ class EventLogisticSaveSerializer(serializers.ModelSerializer):
 
 class EventSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
-    sanggu_hosts = serializers.PrimaryKeyRelatedField(queryset=SangguHost.objects.all(), many=True)
-    office_hosts = serializers.PrimaryKeyRelatedField(queryset=OfficeHost.objects.all(), many=True)
-    org_hosts = serializers.PrimaryKeyRelatedField(queryset=OrgHost.objects.all(), many=True)
     event_logistics = EventLogisticSerializer(many=True, read_only=True)
 
     class Meta:
         model = Event
         fields = ['id', 
-                'name', 
+                'name',
+                'sanggu_hosts',
+                'office_hosts',
+                'org_hosts', 
                 'description', 
                 'poster_url', 
                 'outside_venue_name',
@@ -43,13 +43,13 @@ class CreateEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 
-                'name', 
+                'name',
+                'sanggu_hosts',
+                'office_hosts',
+                'org_hosts',
                 'description', 
                 'poster_url', 
                 'outside_venue_name',
                 'event_url',
-                'tags',
-                'sanggu_hosts',
-                'office_hosts',
-                'org_hosts',
-                'event_logistics']
+                'tags']
+                # 'event_logistics']
