959b7a95a65ed70f1e2911d20c1db47aede33c6a
Miguel N. Galace <galacemiguel@gmail.com>
2018-08-23 01:04:27 +0800

    Add responsive and portrait attributes to MediaCard

--- client/src/components/MediaCard.js
+++ client/src/components/MediaCard.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { media } from '../style/style-utils';
 
 const Card = styled.article`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${props => props.responsive ? 'row' : 'column'};
   height: 100%;
   border-radius: .3em;
   box-shadow: 0 .1em .6em rgba(0,0,0,.16);
   background: #fcfcfc;
+
+  ${media.mdScreen`
+    flex-direction: column;
+  `}
 `;
 
-const CardImage = styled.img`
-  width: 100%;
+const CardImage = styled.div`
+  width: 25%;
+  background: url(${props => props.src}) center center;
+  background-size: cover;
+
+  ${media.mdScreen`
+    width: 100%;
+    padding-top: ${props => props.portrait ? '141%' : '100%'};
+  `}
 `;
 
 const CardTextBox = styled.div`
-  flex-grow: 1;
   padding: 1em;
 `;
 
 class MediaCard extends Component {
   render() {
     return (
-      <Card>
-        <CardImage src={this.props.imgSrc} alt={this.props.imgAlt} />
-        <CardTextBox>
+      <Card
+        className={this.props.className}
+        responsive={this.props.responsive}
+      >
+        <CardImage
+          role="img"
+          aria-label={this.props.imgAlt}
+          title={this.props.imgAlt}
+          responsive={this.props.responsive}
+          src={this.props.imgSrc}
+          portrait={this.props.portrait}
+        />
+        <CardTextBox responsive={this.props.responsive}>
           {this.props.children}
         </CardTextBox>
       </Card>
