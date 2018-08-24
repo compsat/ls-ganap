19433c7e01f5a589af4d41ccbd0a0f9ab4b9e64b
Eddrich Janzzen Ang <eddrichang@gmail.com>
2018-08-24 23:37:37 +0800

    Fix entrypoint

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,29 +2,29 @@
 
 # Apply database migrations
 echo "Create the database migrations"
-python3 ls_ganap/manage.py makemigrations
+python3 manage.py makemigrations
 
 # Apply database migrations
 echo "Apply database migrations"
-python3 ls_ganap/manage.py migrate
+python3 manage.py migrate
 
 # Run fixtures
 echo "Run Fixtures"
-python3 ls_ganap/manage.py loaddata clusters.yaml
-python3 ls_ganap/manage.py loaddata tags.yaml
-python3 ls_ganap/manage.py loaddata org_type.yaml
-python3 ls_ganap/manage.py loaddata event_hosts.yaml
-python3 ls_ganap/manage.py loaddata org_users.yaml
-python3 ls_ganap/manage.py loaddata organizations.yaml
-python3 ls_ganap/manage.py loaddata sanggu_users.yaml
-python3 ls_ganap/manage.py loaddata sanggu.yaml
-python3 ls_ganap/manage.py loaddata test.yaml
-python3 ls_ganap/manage.py loaddata venues.yaml
-python3 ls_ganap/manage.py loaddata mock_events.yaml
+python3 manage.py loaddata clusters.yaml
+python3 manage.py loaddata tags.yaml
+python3 manage.py loaddata org_type.yaml
+python3 manage.py loaddata event_hosts.yaml
+python3 manage.py loaddata org_users.yaml
+python3 manage.py loaddata organizations.yaml
+python3 manage.py loaddata sanggu_users.yaml
+python3 manage.py loaddata sanggu.yaml
+python3 manage.py loaddata test.yaml
+python3 manage.py loaddata venues.yaml
+python3 manage.py loaddata mock_events.yaml
 
 # Create superuser
 echo "Create superuser"
-python ls_ganap/manage.py shell << END
+python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='ls_ganap123@gmail.com'):
@@ -35,6 +35,6 @@ END
 
 # Start server
 echo "Starting server"
-python3 ls_ganap/manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
 
