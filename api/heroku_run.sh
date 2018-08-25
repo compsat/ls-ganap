07f85e4867ce87ff3ad2453e8ae9f6b889376d62
Eddrich Janzzen Ang <eddrichang@gmail.com>
2018-08-25 13:08:19 +0800

    Fix heroku_run.sh

--- api/heroku_run.sh
+++ api/heroku_run.sh
@@ -2,29 +2,29 @@
 
 # Apply database migrations
 echo "Create the database migrations"
-heroku run python3 manage.py makemigrations
+python3 manage.py makemigrations
 
 # Apply database migrations
 echo "Apply database migrations"
-heroku run python3 manage.py migrate
+python3 manage.py migrate
 
 # Run fixtures
 echo "Run Fixtures"
-heroku run python3 manage.py loaddata clusters.yaml
-heroku run python3 manage.py loaddata tags.yaml
-heroku run python3 manage.py loaddata org_type.yaml
-heroku run python3 manage.py loaddata event_hosts.yaml
-heroku run python3 manage.py loaddata org_users.yaml
-heroku run python3 manage.py loaddata organizations.yaml
-heroku run python3 manage.py loaddata sanggu_users.yaml
-heroku run python3 manage.py loaddata sanggu.yaml
-heroku run python3 manage.py loaddata test.yaml
-heroku run python3 manage.py loaddata venues.yaml
-heroku run python3 manage.py loaddata mock_events.yaml
+python3 manage.py loaddata clusters.yaml
+python3 manage.py loaddata tags.yaml
+python3 manage.py loaddata org_type.yaml
+python3 manage.py loaddata event_hosts.yaml
+python3 manage.py loaddata org_users.yaml
+python3 manage.py loaddata organizations.yaml
+python3 manage.py loaddata sanggu_users.yaml
+python3 manage.py loaddata sanggu.yaml
+python3 manage.py loaddata test.yaml
+python3 manage.py loaddata venues.yaml
+python3 manage.py loaddata mock_events.yaml
 
 # Create superuser
 echo "Create superuser"
-heroku run python3 manage.py shell << END
+python3 manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='ls_ganap123@gmail.com'):
