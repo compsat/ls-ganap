86c662465a119175e32f62a4decb11545f3d5e45
Nikki Domingo <nikkiii.domingo@gmail.com>
2018-09-07 11:24:53 +0800

    Fix api connection problems

--- client/src/home/ScrollerSection.js
+++ client/src/home/ScrollerSection.js
@@ -11,52 +11,69 @@ class ScrollerSection extends Component {
     }
   }
 
-  componentDidMount(){
-    var events_url = this.props.api_url
-    
-    axios.get(events_url)
-    .then(function(response) {
-      var results = null
-      if (response.data.results){ // has pagination
-        results = response.data.results
-        console.log(results)
-      } else if (response.data instanceof Array){ // no pagination
-        results = response.data
-      }
+  componentWillMount(){
+    // Format data for events
+    if (this.props.name === 'events'){
+      // get all events
+      var events = this.props.events
+      // iterate through each event
+      for(var event_key in events){
+        var event = events[event_key]
 
-
-      // Add results to this.state
-      for (var key in results){
-        var item = results[key]
-
-        // Format data for events
-        if (this.props.card_type === 'event'){
-          for (var key in item.event_logistics){
-            // Change venue ID to venue name
-            var venueId = item.event_logistics[key].venue
-            item.event_logistics[key].venue = this.props.venues[venueId].name
-
-            // Format date
-            var date = new Date(item.event_logistics[key].date)
-            item.event_logistics[key].date = date.toLocaleDateString("en-US", {month: 'short', day: 'numeric', year: 'numeric'})
-
-            // Format time
-            item.event_logistics[key].start_time = (item.event_logistics[key].start_time).slice(0, 5)
-            item.event_logistics[key].end_time = (item.event_logistics[key].end_time).slice(0, 5)
-          }
-          // Change host ID to event_host name
-          var hostId = item.host
-          item.host = this.props.event_hosts[hostId].name
+        // Change org ID to org name for each org_host
+        for (var orgId_key in event.org_hosts){
+          var orgId = event.org_hosts[orgId_key]
+          event.org_hosts[orgId_key] = this.props.orgs[orgId].name
         }
 
+        // Change sanggu ID to org name for each sanggu_host
+        for (var sangguId_key in event.sanggu_hosts){
+          var sangguId = event.sanggu_hosts[sangguId_key]
+          event.sanggu_hosts[sangguId_key] = this.props.sanggu[sangguId].name
+        }
+
+        // Change sanggu ID to org name for each sanggu_host
+        for (var officeId_key in event.office_hosts){
+          var officeId = event.office_hosts[officeId_key]
+          event.office_hosts[officeId_key] = this.props.offices[officeId].name
+        }
+
+        // Format each in event_logistics
+        for (var logId_key in event.event_logistics){
+          var event_logistic = event.event_logistics[logId_key]
+
+          var date = new Date(event_logistic.date)
+          event_logistic.date = date.toLocaleDateString("en-US", {month: 'short', day: 'numeric', year: 'numeric'})
+          event_logistic.start_time = (event_logistic.start_time).slice(0,5)
+          event_logistic.end_time = (event_logistic.end_time).slice(0,5)
+          event_logistic.venue = this.props.venues[event_logistic.venue].name
+        }
+        
         this.setState(prevState => ({
-          items: prevState.items.concat(item)
+          items: prevState.items.concat(event)
         }));
-        console.log('item', item)
       }
+    }
+    
+    if (this.props.name === 'orgs') {
+      var orgs = this.props.orgs
+      for(var org_key in orgs) {
+        var org = orgs[org_key]
+        this.setState(prevState => ({
+          items: prevState.items.concat(org)
+        }));
+      }
+    } 
 
-    }.bind(this))
-
+    if (this.props.name === 'offices') {
+      var offices = this.props.offices
+      for(var offices_key in offices) {
+        var office = offices[offices_key]
+        this.setState(prevState => ({
+          items: prevState.items.concat(office)
+        }));
+      }
+    } 
   }
 
   render() {
@@ -66,9 +83,7 @@ class ScrollerSection extends Component {
           <Card 
             key={item.id} 
             item={item} 
-            card_type={this.props.card_type} 
-            venues={this.props.venues}
-            event_hosts={this.props.event_hosts} />
+            card_type={this.props.card_type} />
         ))}
       </HorizontalScroller>
     );
