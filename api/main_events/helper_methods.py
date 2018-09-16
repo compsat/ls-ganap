c06ff01fa9e568da9a46dbf2abfd62bf4926fd7d
Daniel <daniel.delrio@obf.ateneo.edu>
2018-09-16 11:27:02 +0800

    Fix is_accepted error in helper_methods

--- api/main_events/helper_methods.py
+++ api/main_events/helper_methods.py
@@ -3,7 +3,7 @@ from django.db.models import Min
 # helper method for getting date range
 def get_dates_between(start_date, end_date, queryset, event_logistics):
     if(start_date is not None) and (end_date is not None):
-        queryset = queryset.filter(is_accepted=True).annotate(date=Min('event_logistics__date')).filter(event_logistics__date__range=[start_date, end_date]).order_by('date')
+        queryset = queryset.filter(is_approved=True).annotate(date=Min('event_logistics__date')).filter(event_logistics__date__range=[start_date, end_date]).order_by('date')
     else:
         raise Http404
 
