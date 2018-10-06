4a52eb241e04a7abf86d67a207a7bd504842573f
Daniel <daniel.delrio@obf.ateneo.edu>
2018-10-07 00:21:54 +0800

    Globally annotate first_date field in models.py

--- api/main_events/helper_methods.py
+++ api/main_events/helper_methods.py
@@ -3,7 +3,7 @@ from django.db.models import Min
 # helper method for getting date range
 def get_dates_between(start_date, end_date, queryset, event_logistics):
     if(start_date is not None) and (end_date is not None):
-        queryset = queryset.filter(is_approved=True).filter(event_logistics__date__range=[start_date, end_date]).annotate(date=Min('event_logistics__date')).order_by('date')
+        queryset = queryset.filter(is_approved=True).filter(event_logistics__date__range=[start_date, end_date]).order_by('first_date')
     else:
         raise Http404
 
