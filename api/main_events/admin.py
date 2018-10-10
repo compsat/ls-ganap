c9305817c5eec5c7a94fedd57e35acbfdb0093fa
Daniel <daniel.delrio@obf.ateneo.edu>
2018-10-10 23:09:28 +0800

    Add list_display and list_filter to User model in admin

--- api/main_events/admin.py
+++ api/main_events/admin.py
@@ -19,6 +19,43 @@ class UserOfficeInline(admin.StackedInline):
 class UserSangguInline(admin.StackedInline):
 	model = SangguHost
 
+class HostTypeFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _('host type')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'type'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('org', _('Org')),
+            ('office', _('Office')),
+            ('sanggu', _('Sanggu')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'org':
+            return queryset.exclude(org_host=None)
+        if self.value() == 'office':
+            return queryset.exclude(office_host=None)
+        if self.value() == 'sanggu':
+            return queryset.exclude(sanggu_host=None)
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
 	"""Define admin model for custom User model with no email field."""
@@ -33,9 +70,30 @@ class UserAdmin(DjangoUserAdmin):
 			'fields': ('email', 'password1', 'password2'),
 		}),
 	)
-	list_display = ('email', 'is_staff')
+	list_filter = (HostTypeFilter, 'is_staff', 'is_superuser', 'is_active')
+	list_display = ('email', 'host_type', 'host_name', 'is_staff')
 	search_fields = ('email',)
 	ordering = ('email',)
+
+	def host_name(self, obj):
+		if hasattr(obj, 'org_host'):
+			return obj.org_host.name
+		elif hasattr(obj, 'office_host'):
+			return obj.office_host.name
+		elif hasattr(obj, 'sanggu_host'):
+			return obj.sanggu_host.name
+
+		return ''
+
+	def host_type(self, obj):
+		if hasattr(obj, 'org_host'):
+			return 'Org'
+		elif hasattr(obj, 'office_host'):
+			return 'Office'
+		elif hasattr(obj, 'sanggu_host'):
+			return 'Sanggu'
+
+		return ''
 
 	def add_org_view(self, request, extra_content=None):
 		self.inlines = [UserOrgInline]
