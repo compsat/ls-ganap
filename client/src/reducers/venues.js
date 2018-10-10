eef7a7006426849f160647743f951bd1f30ed2a6
Miguel N. Galace <galacemiguel@gmail.com>
2018-10-10 22:22:43 +0800

    Write venues fetch logic

---
+++ client/src/reducers/venues.js
@@ -0,0 +1,35 @@
+import {
+  FETCH_VENUES_REQUEST,
+  FETCH_VENUES_SUCCESS,
+  FETCH_VENUES_FAILURE
+} from "../actions/venues";
+
+const venues = (
+  state = {
+    isFetching: false,
+    failedToFetch: false,
+    items: []
+  },
+  action
+) => {
+  switch (action.type) {
+    case FETCH_VENUES_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case FETCH_VENUES_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.venues
+      });
+    case FETCH_VENUES_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        failedToFetch: true
+      });
+    default:
+      return state;
+  }
+};
+
+export default venues;
