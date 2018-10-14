080dfb1a5888a9e06b47c6b9a00ce38722c7f6f5
Miguel N. Galace <galacemiguel@gmail.com>
2018-10-14 09:19:48 +0800

    Fix formatting

--- client/src/browse/BrowseView.js
+++ client/src/browse/BrowseView.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import SearchBar from './SearchBar';
-import FilterBarContainer from '../containers/FilterBarContainer';
-import MediaCard from '../components/MediaCard';
-import AppSubheading from '../components/AppSubheading';
-import AppText from '../components/AppText';
-import { media } from '../style/style-utils';
-import SVG from 'react-inlinesvg';
+import React, { Component } from "react";
+import styled from "styled-components";
+import SearchBar from "./SearchBar";
+import FilterBarContainer from "../containers/FilterBarContainer";
+import MediaCard from "../components/MediaCard";
+import AppSubheading from "../components/AppSubheading";
+import AppText from "../components/AppText";
+import { media } from "../style/style-utils";
+import SVG from "react-inlinesvg";
 
 const SearchHeader = styled.header`
   ${media.mdScreen`
     margin-top: 3em;
     margin-bottom: 4em;
   `}
-`
+`;
 
 const BrowseSearchBar = styled(SearchBar)`
   ${media.mdScreen`
     width: 28em;
     margin: 0 auto;
   `}
-`
+`;
 
 const MainContentBox = styled.div`
   ${media.mdScreen`
@@ -28,7 +28,7 @@ const MainContentBox = styled.div`
     justify-content: space-between;
     align-items: flex-start;
   `}
-`
+`;
 
 const BrowseFilterBar = styled(FilterBarContainer)`
   margin-bottom: 1em;
@@ -39,26 +39,26 @@ const BrowseFilterBar = styled(FilterBarContainer)`
     min-width: 16em;
     margin-right: 2em;
   `}
-`
+`;
 
 const EventItemList = styled.ul`
   flex-grow: 1;
-`
+`;
 
 const SpinnerIcon = styled(SVG)`
   display: block;
   width: 2em;
   margin: 0 auto;
-`
+`;
 
 const BrowseMediaCard = styled(MediaCard)`
   min-height: 12em;
   margin-bottom: 1em;
-`
+`;
 
-const MediaCardHostP = AppText.withComponent('p').extend`
+const MediaCardHostP = AppText.withComponent("p").extend`
   margin-bottom: 1em;
-`
+`;
 
 class BrowseView extends Component {
   componentDidMount() {
@@ -66,51 +66,60 @@ class BrowseView extends Component {
     this.props.fetchEvents();
   }
 
-  getItemProp = (arr, id, prop, fallbackValue)  => {
-    const item = arr.find((item) => item.id === id);
+  getItemProp = (arr, id, prop, fallbackValue) => {
+    const item = arr.find(item => item.id === id);
 
     return item ? item[prop] : fallbackValue;
-  }
+  };
 
-  formatHosts = (hostIds) => {
-    const hostNames = hostIds.map((hostId) => (
+  formatHosts = hostIds => {
+    const hostNames = hostIds.map(hostId =>
       this.getItemProp(this.props.hosts.items, hostId, "name")
-    ));
+    );
 
-    return `Hosted by ${hostNames[0]} and ${hostNames.length - 1} other${hostNames.length - 1 > 1 && 's'}`
-  }
+    return `Hosted by ${hostNames[0]} and ${hostNames.length -
+      1} other${hostNames.length - 1 > 1 && "s"}`;
+  };
 
   formatDate(date) {
     const dateObject = new Date(date);
     const dateString = dateObject.toDateString().substr(4);
 
-    return dateString.replace(/ (?=[^ ]*$)/, ', ');
+    return dateString.replace(/ (?=[^ ]*$)/, ", ");
   }
 
   formatTime(time) {
-    const [hours, mins] = time.split(':');
+    const [hours, mins] = time.split(":");
     const timeStringOptions = {
       hour12: true,
-      hour: 'numeric',
-      minute: '2-digit',
+      hour: "numeric",
+      minute: "2-digit"
     };
     const timeContainer = new Date();
 
     timeContainer.setHours(hours, mins);
 
-    return timeContainer.toLocaleTimeString('en-US', timeStringOptions);
+    return timeContainer.toLocaleTimeString("en-US", timeStringOptions);
   }
 
   render() {
-    return <main>
+    return (
+      <main>
         <SearchHeader>
           <BrowseSearchBar />
         </SearchHeader>
         <MainContentBox>
           <BrowseFilterBar />
-          {!this.props.events.isFetching ? <EventItemList>
-              {this.props.events.items.map(event => <li key={event.id}>
-                  <BrowseMediaCard portrait horizontal imgSrc={event.poster_url} imgAlt={event.name}>
+          {!this.props.events.isFetching ? (
+            <EventItemList>
+              {this.props.events.items.map(event => (
+                <li key={event.id}>
+                  <BrowseMediaCard
+                    portrait
+                    horizontal
+                    imgSrc={event.poster_url}
+                    imgAlt={event.name}
+                  >
                     <AppSubheading size="1">{event.name}</AppSubheading>
                     <MediaCardHostP>
                       {this.formatHosts([
@@ -132,13 +141,15 @@ class BrowseView extends Component {
                       )}
                     </p>
                   </BrowseMediaCard>
-                </li>)}
-            </EventItemList> : (
-              <SpinnerIcon src={require("../assets/icon-spinner.svg")} />
-            )
-          }
+                </li>
+              ))}
+            </EventItemList>
+          ) : (
+            <SpinnerIcon src={require("../assets/icon-spinner.svg")} />
+          )}
         </MainContentBox>
-      </main>;
+      </main>
+    );
   }
 }
 
