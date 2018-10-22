ab34bf2f3ebe6b19e3bddeb672ad4702b6305b7e
Eddrich Janzzen Ang <eddrichang@gmail.com>
2018-10-22 22:34:06 +0800

    Add update password view

--- api/main_events/views/user_view.py
+++ api/main_events/views/user_view.py
@@ -5,9 +5,10 @@ from rest_framework import generics
 from rest_framework.response import Response
 # import the pagination settings
 from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumberPagination
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from main_events.serializers import user_serializer
+from main_events.jwt_authentication import MyJWTAuthentication
 
 
 User = get_user_model()
@@ -33,3 +34,32 @@ class UserLoginAPIView(APIView):
 			new_data = serializer.data
 			return Response(new_data, status=HTTP_200_OK)
 		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class UpdatePassword(APIView):
+	serializer_class = user_serializer.ChangePasswordSerializer
+	authentication_classes = [MyJWTAuthentication,]
+	permission_classes = [IsAuthenticated,]
+
+	def get_object(self, queryset=None):
+		return self.request.user
+
+	def put(self, request, *args, **kwargs):
+		self.object = self.get_object()
+		serializer = user_serializer.ChangePasswordSerializer(data=request.data)
+
+		if serializer.is_valid():
+			old_password = serializer.data.get("old_password")
+			# print(self.object)
+			if not self.object.check_password(old_password):
+				return Response({"old_password": ["wrong password"]}, status=HTTP_400_BAD_REQUEST)
+
+			self.object.set_password(serializer.data.get("new_password"))
+			self.object.save()
+			return Response("Password for user: {} changed.".format(self.object), status=HTTP_200_OK)
+
+		return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+
+
