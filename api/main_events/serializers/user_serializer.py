b87431b307fb1d9c9bc73b3988f2f25b3f9af148
Eddrich Janzzen Ang <eddrichang@gmail.com>
2018-10-22 22:33:20 +0800

    add update password serializer

--- api/main_events/serializers/user_serializer.py
+++ api/main_events/serializers/user_serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
@@ -86,5 +87,24 @@ class UserLoginSerializer(serializers.ModelSerializer):
 
 
         return data
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
+
+
+
+
+
+
+
+
+
 
 
