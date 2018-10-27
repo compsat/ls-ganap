3a9b4179e3af3f035bdb369d48b4b8ad60c9524e
Daniel <daniel.delrio@obf.ateneo.edu>
2018-10-27 20:22:40 +0800

    Allow authenticated users to create non-duplicate tags and add search query

--- api/main_events/serializers/tag_serializer.py
+++ api/main_events/serializers/tag_serializer.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from main_events.serializers import event_serializer
 from main_events.models import Tag
+from django.core.exceptions import ValidationError
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id',
                   'name']
+
+    def validate(self, data):
+        name = data['name']
+
+        if not name:
+          raise Validation('A tag is not given.')
+
+        tag = Tag.objects.filter(name__iexact=name).distinct()
+
+        if tag.exists():
+          raise ValidationError('This tag already exists.')
+
+        return data
 
 class TagDetailSerializer(serializers.ModelSerializer):
     event_list = event_serializer.EventSerializer(many=True, read_only=True)
--- api/main_events/views/tag_view.py
+++ api/main_events/views/tag_view.py
@@ -7,19 +7,27 @@ from rest_framework.response import Response
 # import the pagination settings
 from main_events.pagination import ObjectLimitOffsetPagination, ObjectPageNumberPagination
 from rest_framework import status
-
+from main_events.jwt_authentication import MyJWTAuthentication
+from rest_framework.filters import SearchFilter
 
 class TagList(APIView):
     """
     get: List all the tags.
     """
     serializer_class = TagSerializer
+    authentication_classes = [MyJWTAuthentication,]
+    filter_backends = [SearchFilter]
     # specifies which pagination settings to follow
 
     def get(self, request, format=None):
         queryset = Tag.objects.all()
         pagination_class = ObjectPageNumberPagination
         paginator = pagination_class()
+
+        search = self.request.GET.get("search")
+        
+        if search:
+            queryset = queryset.filter(name__icontains=search).distinct()
 
         if request.method == 'GET' and 'page' in request.GET:
 
@@ -32,6 +40,13 @@ class TagList(APIView):
             serializer =  TagSerializer(queryset, many=True)
             
             return Response({"results" : serializer.data})
+
+    def post(self, request, format=None):
+        serializer = TagSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagDetail(generics.RetrieveAPIView):
