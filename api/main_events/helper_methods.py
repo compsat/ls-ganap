de32301ffbd14dda4b90f14b7957c3822ab5ec97
Daniel <daniel.delrio@obf.ateneo.edu>
2018-12-27 19:03:43 +0800

    Make helper method for returning filtered queryset

--- api/main_events/helper_methods.py
+++ api/main_events/helper_methods.py
@@ -1,4 +1,5 @@
 from django.db.models import Min
+from django.db.models import Q
 
 # helper method for getting date range
 def get_dates_between(start_date, end_date, queryset, event_logistics):
@@ -8,3 +9,20 @@ def get_dates_between(start_date, end_date, queryset, event_logistics):
         raise Http404
 
     return queryset
+
+def tags_hostgroup_filter(queryset, request, host_map):
+	host_query = request.query_params.get('host_query', None)
+	tags = request.query_params.get('tags', None)
+
+	if host_query:
+		queryset = queryset.filter(host_map[host_query]).distinct()
+
+	if tags:
+		tags_list = tags.split(',')
+		queries = Q()
+		for tag in tags_list:
+			queries = queries | Q(tags__pk=tag)
+
+		queryset = queryset.filter(queries).distinct()
+
+	return queryset
