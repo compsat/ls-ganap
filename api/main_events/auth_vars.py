432caa2a49e7f4f89dd67022afcea59e7570d5fb
Daniel <daniel.delrio@obf.ateneo.edu>
2018-12-29 14:39:00 +0800

    Store IDs of calendars in auth_vars.py

---
+++ api/main_events/auth_vars.py
@@ -0,0 +1,412 @@
+host_calendar_ids = {
+	1 : "0726ffaurhufimpoq0lfcjlkns@group.calendar.google.com",
+	2 : "dsekcjc4ugh0j44a4jkuq4ldkk@group.calendar.google.com",
+	3 : "5rnbodbhi3r7cnstv1aaecp068@group.calendar.google.com",
+	4 : "g2m4knupgjk47hqjsb9f6svots@group.calendar.google.com",
+	5 : "smt22775pf0e1iut2hif38b614@group.calendar.google.com",
+	6 : "8nm9oao8c8d7gt1rj29dilmu0g@group.calendar.google.com",
+	7 : "1uk06qkuh70crvpkr4fk7oct7s@group.calendar.google.com",
+	8 : "njmtdie2pqh60a44jdmk6ug43k@group.calendar.google.com",
+	9 : "hk5oam2vmpseui1br47jfp012c@group.calendar.google.com",
+	10 : "vc4ku0jucnl061hnv9uoa1jse0@group.calendar.google.com",
+	11 : "priohue7aun814rhm2hioug6ak@group.calendar.google.com",
+	12 : "0hlcoevdc1n95v2gulu7eoh5ik@group.calendar.google.com",
+	13 : "v5d9bh2o42l6jp8p9gi1bcflbo@group.calendar.google.com",
+	14 : "ccrgpipurb6mtjg79r1hm5bvdo@group.calendar.google.com",
+	15 : "1ajgvakkgng3j3indc6hugse78@group.calendar.google.com",
+	16 : "vru6jf2un35n8c6rao7u5v6cgc@group.calendar.google.com",
+	17 : "5og5t98a7sa1efbjq861oi7l3c@group.calendar.google.com",
+	18 : "tnb37v7fc24sa2ta0o4rc90ej8@group.calendar.google.com",
+	19 : "9i7d8su0svo3m4ev5i8trpohj4@group.calendar.google.com",
+	20 : "p1esq2ckkjt9284872psvlsqfo@group.calendar.google.com",
+	21 : "vrdkmpi58cuj56trlqhl4tek5s@group.calendar.google.com",
+	22 : "3b6isana5kqtfl184pjgunbfmo@group.calendar.google.com",
+	23 : "c57lunqevrpajkl3f4e9lfmh4s@group.calendar.google.com",
+	24 : "jr1o6k8pd1a7gvi73jot4so79c@group.calendar.google.com",
+	25 : "tdib2nslj7vi12t2qb9hsnf0vk@group.calendar.google.com",
+	26 : "4k1suu17j4h2rklrtf8ius5334@group.calendar.google.com",
+	27 : "t5ncbb3pd05k25dunvnm2ultg4@group.calendar.google.com",
+	28 : "10v8culaoosntguq8fnkb551gc@group.calendar.google.com",
+	29 : "0qqjk0o72jdfos0kfagc3h59d0@group.calendar.google.com",
+	30 : "sqr1t7kn9m12frg9rnmmcsn7o4@group.calendar.google.com",
+	31 : "rlrphhuunuut7ojhpctom4ntr8@group.calendar.google.com",
+	32 : "d0gho9mh08qbneh044q2c1b920@group.calendar.google.com",
+	33 : "ah91a2gh2tddcb0gchnnsd7k5k@group.calendar.google.com",
+	34 : "ghb7jlpns0gtd01qcsdllfr3cc@group.calendar.google.com",
+	35 : "mb6rh0agg17nelnmvbsa5qmcek@group.calendar.google.com",
+	36 : "somuah46qq81tip8vo4d53btmo@group.calendar.google.com",
+	37 : "q29ii7vdisdur0idauhi0ne4kk@group.calendar.google.com",
+	38 : "v24hu4n88s50lmtjd8jqpent0k@group.calendar.google.com",
+	39 : "9hpc1n5rrmjglumpj4e2f4v83g@group.calendar.google.com",
+	40 : "2jgjk0s85dnphulc56tceog20c@group.calendar.google.com",
+	41 : "v6gfa7g480lec5p04dsus1p0dc@group.calendar.google.com",
+	42 : "1qjasq068jipkpt6urkkv7itsc@group.calendar.google.com",
+	43 : "ht3suvt65uogor70iv9g7o3pp8@group.calendar.google.com",
+	44 : "amaukcpd6f0o32ael8vgpbnnck@group.calendar.google.com",
+	45 : "36rm4j8opn67tkdqufbh2ikr78@group.calendar.google.com",
+	46 : "88m58c14olvompri9aeqn7telk@group.calendar.google.com",
+	47 : "qb05hskcppvb0809j547li63no@group.calendar.google.com",
+	48 : "03ovd0f7pvk7shfont734mfabs@group.calendar.google.com",
+	49 : "l30kb7j94n8as23gsdse65siho@group.calendar.google.com",
+	50 : "3stcookbrfi4ecc989okipna2s@group.calendar.google.com",
+	51 : "qvehq3epk5tecp4iqehi2qcqs0@group.calendar.google.com",
+	52 : "ubcd0om3n8cu3hbjdlq30e7pc8@group.calendar.google.com",
+	53 : "bbikjcv2a3k2fe34gvc9d3ut1g@group.calendar.google.com",
+	54 : "t1upulipel6ulh4r6gmt21r8e0@group.calendar.google.com",
+	55 : "0f8k7k5pgsont49vvcobomtb3k@group.calendar.google.com",
+	56 : "buqqt0jqnosruv7gq8iqi1o0nk@group.calendar.google.com",
+	57 : "65amfc4ndehl6e1tse3sq2858o@group.calendar.google.com",
+	58 : "nepq1nqmri0hil21i97omm9q84@group.calendar.google.com",
+	59 : "953r9fo5hts9vpoo158em12fs8@group.calendar.google.com",
+	60 : "gcqddd9jef79kcus9qq15u07kc@group.calendar.google.com",
+	61 : "biqi2qgotvi7e81231me15t68k@group.calendar.google.com",
+	62 : "g2vr4mh61p5vbqgm1tcnum1eek@group.calendar.google.com",
+	63 : "pb3mbfere8rrcgjgtvo5qpnvro@group.calendar.google.com",
+	64 : "orn2nep9r4u3qv58mavoe2cqek@group.calendar.google.com",
+	65 : "tjrrqtp89ar3vr2p4jkomobia4@group.calendar.google.com",
+	66 : "id140kb4vd2a64r8jkeeqbv98o@group.calendar.google.com",
+	67 : "r36bodcf87gqpbdhrqnag9iou0@group.calendar.google.com",
+	68 : "lnv2elroe3becbagubuqmpna4s@group.calendar.google.com",
+	69 : "b6nlcr862ak48r7ocl2gp6jnvs@group.calendar.google.com",
+	70 : "01t33bvg3el6me7bg653qbj970@group.calendar.google.com",
+	71 : "nc43ir64dh36jbn4g9huae1ss0@group.calendar.google.com",
+	72 : "q49v2rqcc6a5onh87s344a320o@group.calendar.google.com",
+	73 : "0kbc3kgte3krviou6uusg6hos4@group.calendar.google.com",
+	74 : "ladvgcpct6sig7i4rm7v5a2igg@group.calendar.google.com",
+	75 : "30hou85mvoc2rlvng72kdkf8f4@group.calendar.google.com",
+	76 : "kr36jc3gnmaa3ejlcnq2anq0uc@group.calendar.google.com",
+	77 : "u82ll0uct5061chg1t8gn91edk@group.calendar.google.com",
+	78 : "8ikndk6hcdod8mvuo5s4p9vpjs@group.calendar.google.com",
+	79 : "95hqnbq5mifhhmpigovind0fl4@group.calendar.google.com",
+	80 : "701ojjb9lighktk5cpth7cbelk@group.calendar.google.com",
+	81 : "sb1lqnv3971fdr2gjcetjpgph8@group.calendar.google.com",
+	82 : "nct1f7dios9rg0ec250nkpng7k@group.calendar.google.com",
+	83 : "1h4e0l6k0o5pim39u096qclr84@group.calendar.google.com",
+	84 : "n123j4au3ju5j2mk44eopcdlog@group.calendar.google.com",
+	85 : "8skj6obiu9pubjb3q6lqfp8838@group.calendar.google.com",
+	86 : "8skj6obiu9pubjb3q6lqfp8838@group.calendar.google.com",
+	87 : "n8lbm71i9ns6ce3kogh90al7dg@group.calendar.google.com",
+	88 : "slp4pouhicstv0d64vc7qq936s@group.calendar.google.com",
+	89 : "i6f62opejscdu62pcsdi6ile3g@group.calendar.google.com",
+	90 : "76re590ifu00jd8mih66ml0isg@group.calendar.google.com",
+	91 : "n768ffvgh533b602epvqluee0g@group.calendar.google.com",
+	92 : "n647dmma39f4i292nmaivkue54@group.calendar.google.com",
+	93 : "r827dsa4n8fou32mgnmg4iq7i0@group.calendar.google.com",
+	94 : "gib5fd1agdtaqlvbn9d3jq7tkg@group.calendar.google.com",
+	95 : "rc8airgjosg7cb6vru1bmk7grc@group.calendar.google.com",
+	96 : "97r75j3v8n8ol1o7e683ih1ig4@group.calendar.google.com",
+	97 : "37e02nu4bkp8ingh7tlc1gf65o@group.calendar.google.com",
+	98 : "lf8s9vqor4d82l0cvphglnbhto@group.calendar.google.com",
+	99 : "jmbtd5ufb44lvm1a0i6r11nij4@group.calendar.google.com",
+	100 : "t6r2ubn86pdv7pniv8akfuo41k@group.calendar.google.com",
+	101 : "sh48ooh2pgv5n7rqvkllju2eoo@group.calendar.google.com",
+	102 : "gqebc2qseaorb9i55d2cuvefbs@group.calendar.google.com",
+	103 : "rfvuopffsag0ll96gib49do2ho@group.calendar.google.com",
+	104 : "0uj6iutguepes3nenmoa7a6fn0@group.calendar.google.com",
+	105 : "a4shm2sb83or660f962soen1m0@group.calendar.google.com",
+	106 : "mgrbvg1193kc9qj1mp54t1p0ts@group.calendar.google.com",
+	107 : "anp9fpe9lgk3t2rgvrfn7cqijs@group.calendar.google.com",
+	108 : "ut6ctqri5cj37u1ip0nd8q5o8g@group.calendar.google.com",
+	109 : "qge0u3cnn7clq2t00u2tc0l7n0@group.calendar.google.com",
+	110 : "m85tkvl8am3re5tqfb6l34te14@group.calendar.google.com",
+	111 : "ec3i0qq6bn8v0amf9tlk3qoeto@group.calendar.google.com",
+	112 : "j6ms3s28qdrg4arf4k5r0uul3k@group.calendar.google.com",
+	113 : "llos1sio3ku4e78hteqtmr2ss4@group.calendar.google.com",
+	114 : "htr8iqn5bdgi8vr3c0irlrfvnc@group.calendar.google.com",
+	115 : "1h3mnteg2sno3d3fakq1no9j08@group.calendar.google.com",
+	116 : "hllecm9vgq8okt8mhlsjiv5ok0@group.calendar.google.com",
+	117 : "g2ie65kdva29l67jprm95petlo@group.calendar.google.com",
+	118 : "a059jh6nku2vm9oqhnomtf47ec@group.calendar.google.com",
+	119 : "1tqpm3vejm19qsn6n6ed0rag90@group.calendar.google.com",
+	120 : "30s0mvdr3hnfuqfrhe8aue2v24@group.calendar.google.com",
+	121 : "fl0t2d2pb2lmff82l1adup06u8@group.calendar.google.com",
+	122 : "4s78s75lufh6h4dp9pb6uu3rc4@group.calendar.google.com",
+	123 : "4ucm0imhmkifucior0ed6gara4@group.calendar.google.com",
+	124 : "cmhe4imdf9vkf5ca4tg22dab34@group.calendar.google.com",
+	125 : "mgn591p07921fodv8222d6akog@group.calendar.google.com",
+	126 : "io2vrhtp6q091go9kl408dik6g@group.calendar.google.com",
+	127 : "mfl2otuq78bl7ta0nauvh18vmc@group.calendar.google.com",
+	128 : "hmsag48rrkl245jie9qgg93vf4@group.calendar.google.com",
+	129 : "8aft3m6ug8ps0t7nqc94ks5sb4@group.calendar.google.com",
+	130 : "fv45d5t6s408vcur3m0aur6tk4@group.calendar.google.com",
+	131 : "4cag1ufeeodlujdveo1pum6ufo@group.calendar.google.com",
+}
+
+"""
+Syncs the Google Calendar of a user to the calendar of a host, given its pk.
+"""
+# def sync_host(request, host_type, pk):
+# 	if 'credentials' not in request.session or request.session['credentials'] is None:
+# 		"""
+# 		Instead of passing the pk as parameters to the views,
+# 		I just stored the pk in the session.
+# 		"""
+# 		request.session['pk'] = pk
+# 		request.session['endpoint'] = 'sync_host'
+# 		request.session['host_type'] = host_type
+# 		return redirect('authorize')
+	
+# 	  # Load credentials from the session.
+# 	credentials = google.oauth2.credentials.Credentials(
+# 		**request.session['credentials'])
+
+# 	if credentials.expired:
+# 		try:
+# 			credentials.refresh(request)
+# 		except:
+# 			request.session['credentials'] = None
+# 			request.session['pk'] = pk
+# 			return redirect('authorize')
+
+# 	service = build('calendar', 'v3', credentials=credentials)
+
+# 	first_date = None
+# 	auth_user = None
+
+# 	try:
+# 		event_host = None
+# 		events = None
+# 		if host_type == 'sanggu':
+# 			event_host = SangguHost.objects.get(pk=pk)
+# 			events = Event.objects.filter(sanggu_hosts=pk, is_approved=True)
+# 		elif host_type == 'orgs':
+# 			event_host = OrgHost.objects.get(pk=pk)
+# 			events = Event.objects.filter(org_hosts=pk, is_approved=True)
+# 		elif host_type == 'offices':
+# 			event_host = OfficeHost.objects.get(pk=pk)
+# 			events = Event.objects.filter(office_hosts=pk, is_approved=True)
+# 		else:
+# 			return redirect('index')
+
+# 		calendar = {
+# 		    'summary': event_host.name,
+# 		    'timeZone': 'Asia/Manila'
+# 		}
+# 		host_calendar = service.calendars().insert(body=calendar).execute()
+
+# 		for event in events:
+# 			event_logistics = EventLogistic.objects.filter(event=event.pk)
+# 			for logistic in event_logistics:
+# 				start_time = datetime.combine(logistic.date, logistic.start_time)
+# 				if first_date is None:
+# 					first_date = logistic.date
+# 				end_time = datetime.combine(logistic.date, logistic.end_time)
+# 				location = None
+# 				if logistic.venue:
+# 					location = logistic.venue.name
+# 				else:
+# 					location = logistic.outside_venue_name
+					
+# 				EVENT = {
+# 					"summary": event.name,
+# 					"location": location,
+# 					"description": event_instance.description,
+# 					"start": {'dateTime': start_time.isoformat(), 'timeZone': 'Asia/Manila'},
+# 					"end": {'dateTime': end_time.isoformat(), 'timeZone': 'Asia/Manila'}
+# 				}
+# 				eventTest = service.events().insert(calendarId=host_calendar['id'], body=EVENT).execute()
+# 				auth_user = eventTest['creator']['email']
+
+# 		# Save credentials back to session in case access token was refreshed.
+# 		# ACTION ITEM: In a production app, you likely want to save these
+# 		#              credentials in a persistent database instead.
+# 		request.session['credentials'] = credentials_to_dict(credentials)
+# 	except RefreshError:
+# 		request.session['pk'] = pk
+# 		return redirect('authorize')
+
+# 	request.session['pk'] = None
+# 	if auth_user:
+# 		return redirect('https://calendar.google.com/calendar/r/day/{}/{}/{}?authuser={}'.format(first_date.year, first_date.month, first_date.day, auth_user))
+# 	else:
+# 		return redirect('https://calendar.google.com/calendar/')
+
+# def get_calendar(request):
+# 	# info = json.loads(service_secrets)
+# 	# credentials = service_account.Credentials.from_service_account_info(
+# 	# 	info, scopes=SCOPES)
+# 	if 'credentials' not in request.session or request.session['credentials'] is None:
+# 		"""
+# 		Instead of passing the pk as parameters to the views,
+# 		I just stored the pk in the session.
+# 		"""
+# 		# request.session['pk'] = pk
+# 		request.session['endpoint'] = 'sync_host'
+# 		request.session['host_type'] = host_type
+# 		return redirect('authorize')
+	
+# 	  # Load credentials from the session.
+# 	credentials = google.oauth2.credentials.Credentials(
+# 		**request.session['credentials'])
+
+# 	if credentials.expired:
+# 		try:
+# 			credentials.refresh(request)
+# 		except:
+# 			request.session['credentials'] = None
+# 			# request.session['pk'] = pk
+# 			return redirect('authorize')
+
+
+# 	service = build('calendar', 'v3', credentials=credentials)
+
+# 	page_token = None
+# 	while True:
+# 		calendar_list = service.calendarList().list(pageToken=page_token).execute()
+# 		orgs = OfficeHost.objects.all()
+# 		# sanggu = SangguHost.objects.all()
+# 		org_list = []
+# 		for calendar_list_entry in calendar_list['items']:
+# 			acl = service.acl().list(calendarId=calendar_list_entry['id']).execute()
+# 			defaultAlready = False
+# 			for rule in acl['items']:
+# 				if rule['scope']['type'] == 'default' and rule['role'] == 'reader':
+# 					defaultAlready = True
+# 					break
+# 			if not defaultAlready:
+# 				rule = {
+# 					'scope' : {
+# 						'type' : 'user',
+# 						'value' : 'ls-ganap-calendars@ls-ganap-225801.iam.gserviceaccount.com',
+# 					},
+# 					'role' : 'writer'
+# 				}
+# 				created_rule = service.acl().insert(calendarId=calendar_list_entry['id'], body=rule).execute()
+# 				default_rule = {
+# 					'scope' : {
+# 						'type' : 'default',
+# 					},
+# 					'role' : 'reader'
+# 				}
+# 				created_default_rule = service.acl().insert(calendarId=calendar_list_entry['id'], body=default_rule).execute()
+# 			for org in orgs:
+# 				if org.name == calendar_list_entry['summary']:
+# 					org_list.append((org.pk, calendar_list_entry['id']))
+# 					break
+# 			# for org in sanggu:
+# 			# 	if org.name == calendar_list_entry['summary']:
+# 			# 		org_list.append((org.pk, calendar_list_entry['id']))
+# 			# 		break
+# 			# print(calendar_list_entry['summary'])
+# 		org_list = sorted(org_list)
+# 		print(org_list)
+# 		file = open('calendar_ids.txt', 'w')
+# 		for idx, cId in org_list:
+# 			file.write('{} : "{}",\n'.format(idx, cId))
+# 		page_token = calendar_list.get('nextPageToken')
+# 		if not page_token:
+# 			break
+
+# 	return redirect('https://calendar.google.com/calendar/')
+
+# def add_calendars(request):
+# 	if 'credentials' not in request.session or request.session['credentials'] is None:
+# 		"""
+# 		Instead of passing the pk as parameters to the views,
+# 		I just stored the pk in the session.
+# 		"""
+# 		# request.session['pk'] = pk
+# 		request.session['endpoint'] = 'sync_host'
+# 		# request.session['host_type'] = host_type
+# 		return redirect('authorize')
+	
+# 	  # Load credentials from the session.
+# 	credentials = google.oauth2.credentials.Credentials(
+# 		**request.session['credentials'])
+
+# 	if credentials.expired:
+# 		try:
+# 			credentials.refresh(request)
+# 		except:
+# 			request.session['credentials'] = None
+# 			# request.session['pk'] = pk
+# 			return redirect('authorize')
+
+# 	service = build('calendar', 'v3', credentials=credentials)
+
+# 	first_date = None
+# 	auth_user = None
+
+# 	try:
+# 		file = open('calendar_offices.txt', 'w')
+# 		orgs = OfficeHost.objects.all().order_by('pk')
+# 		for idx, host in enumerate(orgs, start=68):
+# 			if idx not in host_calendar_ids:
+# 				calendar = {
+# 				    'summary': host.name,
+# 				    'timeZone': 'Asia/Manila'
+# 				}
+# 				host_calendar = service.calendars().insert(body=calendar).execute()
+# 				file.write('{} : "{}",\n'.format(idx, host_calendar['id']))
+# 				print(str(idx) + " : " + host_calendar['id'] + ",")
+# 				rule = {
+# 					'scope' : {
+# 						'type' : 'user',
+# 						'value' : 'ls-ganap-calendars@ls-ganap-225801.iam.gserviceaccount.com',
+# 					},
+# 					'role' : 'writer'
+# 				}
+# 				created_rule = service.acl().insert(calendarId=host_calendar['id'], body=rule).execute()
+# 				default_rule = {
+# 					'scope' : {
+# 						'type' : 'default',
+# 					},
+# 					'role' : 'reader'
+# 				}
+# 				created_default_rule = service.acl().insert(calendarId=host_calendar['id'], body=default_rule).execute()
+
+# 		# Save credentials back to session in case access token was refreshed.
+# 		# ACTION ITEM: In a production app, you likely want to save these
+# 		#              credentials in a persistent database instead.
+# 		request.session['credentials'] = credentials_to_dict(credentials)
+# 	except RefreshError:
+# 		# request.session['pk'] = pk
+# 		return redirect('authorize')
+
+# 	return redirect('https://calendar.google.com/calendar/')
+
+# def del_calendars(request):
+# 	# # Load credentials from the session.
+# 	# info = json.loads(service_secrets)
+# 	# # print(info)
+# 	# credentials = service_account.Credentials.from_service_account_info(
+# 	# 	info, scopes=SCOPES)
+
+# 	# service = build('calendar', 'v3', credentials=credentials)
+
+# 	if 'credentials' not in request.session or request.session['credentials'] is None:
+# 		"""
+# 		Instead of passing the pk as parameters to the views,
+# 		I just stored the pk in the session.
+# 		"""
+# 		# request.session['pk'] = pk
+# 		request.session['endpoint'] = 'sync_host'
+# 		# request.session['host_type'] = host_type
+# 		return redirect('authorize')
+	
+# 	  # Load credentials from the session.
+# 	credentials = google.oauth2.credentials.Credentials(
+# 		**request.session['credentials'])
+
+# 	if credentials.expired:
+# 		try:
+# 			credentials.refresh(request)
+# 		except:
+# 			request.session['credentials'] = None
+# 			# request.session['pk'] = pk
+# 			return redirect('authorize')
+
+# 	service = build('calendar', 'v3', credentials=credentials)
+
+# 	page_token = None
+# 	while True:
+# 		calendar_list = service.calendarList().list(pageToken=page_token).execute()
+# 		for calendar_list_entry in calendar_list['items']:
+# 			if calendar_list_entry['id'] not in host_calendar_ids.values() and calendar_list_entry['id'] != 'lsganapcalendars@gmail.com':
+# 				calendar = service.calendars().delete(calendarId=calendar_list_entry['id']).execute()
+# 				# print(calendar['summary'])
+# 		page_token = calendar_list.get('nextPageToken')
+# 		if not page_token:
+# 			break
+
+# 	return redirect('https://calendar.google.com/calendar/?authuser=lsganapcalendars@gmail.com')
