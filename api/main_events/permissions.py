f49b6941b464c40f4479dc9df2067c2132f90e5a
Daniel <daniel.delrio@obf.ateneo.edu>
2019-02-05 00:22:26 +0800

    Make event editable by creator only

--- api/main_events/permissions.py
+++ api/main_events/permissions.py
@@ -33,3 +33,24 @@ class IsEventHostOrReadOnly(permissions.BasePermission):
             return obj.sanggu_hosts.filter(user=request.user).exists()
 
         return False
+
+class IsCreatorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if hasattr(request.user, "org_host"):
+            return obj.created_by == request.user.org_host.pk
+
+        if hasattr(request.user, "office_host"):
+            return obj.created_by == request.user.office_host.pk
+        
+        if hasattr(request.user, "sanggu_host"):
+            return obj.created_by == request.user.sanggu_host.pk
+
+        return False
