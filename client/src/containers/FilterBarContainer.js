d575d20a7ee200ed4ac6acc2735cb2a4757e19fb
Miguel N. Galace <galacemiguel@gmail.com>
2019-02-05 22:55:53 +0800

    Make FilterBar display host abbreviation instead

--- client/src/containers/FilterBarContainer.js
+++ client/src/containers/FilterBarContainer.js
@@ -3,13 +3,13 @@ import { connect } from "react-redux";
 import FilterBar from "../browse/FilterBar";
 
 const mapStateToProps = state => ({
-  host: state.filters.host.name,
+  host: state.filters.host.abbreviation || state.filters.host.name,
   tags: state.entities.tags.items
-    .filter((tag) => {
+    .filter(tag => {
       return state.filters.tags.includes(tag.id);
     })
-    .map((tag) => tag.name)
-    .join(', '),
+    .map(tag => tag.name)
+    .join(", "),
   dateRange: state.filters.dateRange.key
 });
 
