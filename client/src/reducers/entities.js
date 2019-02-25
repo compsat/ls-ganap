958a28f52acee8e66a9489a5bc52a264761fd97a
Miguel N. Galace <galacemiguel@gmail.com>
2019-02-26 00:37:32 +0800

    Refactor home page

--- client/src/reducers/entities.js
+++ client/src/reducers/entities.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import orgTypes from "reducers/orgTypes";
 import clusters from "reducers/clusters";
+import offices from "reducers/offices";
 import events from "reducers/events";
 import venues from "reducers/venues";
 import hosts from "reducers/hosts";
@@ -9,6 +10,7 @@ import tags from "reducers/tags";
 export default combineReducers({
   orgTypes,
   clusters,
+  offices,
   events,
   venues,
   hosts,
