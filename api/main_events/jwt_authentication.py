002a246dd71c2a4c2d1574716ec0f28a0c91f17d
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-03 21:44:43 +0800

    Return email in payload response of JWT Auth

--- api/main_events/jwt_authentication.py
+++ api/main_events/jwt_authentication.py
@@ -1,6 +1,12 @@
 from rest_framework_jwt import authentication
+from main_events.serializers.user_serializer import UserSerializer
 
 class MyJWTAuthentication(authentication.JSONWebTokenAuthentication):
     user_model = 'main_events.User'
 
 
+def jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'email': user.email
+    }
