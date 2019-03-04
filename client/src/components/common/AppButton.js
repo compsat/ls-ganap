bf27df1b081440c0772958dbf8d67fec92073121
Miguel N. Galace <galacemiguel@gmail.com>
2019-03-04 21:14:31 +0800

    Implement login page styles

--- client/src/components/common/AppButton.js
+++ client/src/components/common/AppButton.js
@@ -1,10 +1,6 @@
 import { css } from "styled-components";
 import AppText from "components/common/AppText";
 
-const AppButtonActive = css`
-  box-shadow: inset 0 0 999em rgba(0, 0, 0, 0.5);
-`;
-
 const AppButton = AppText.withComponent("button").extend`
   display: inline-block;
   padding-top: .6em;
@@ -15,16 +11,8 @@ const AppButton = AppText.withComponent("button").extend`
   border-radius: .25em;
   margin-right: .5em;
   margin-bottom: .5em;
-  ${props =>
-    props.empty
-      ? `
-    background: none;
-    color: #404937;
-  `
-      : `
-    background: #404937;
-    color: ${props.theme.colors.white};
-  `}
+  background: ${props => (props.empty ? "none" : "#404937")};
+  color: ${props => (props.empty ? "#404937" : props.theme.colors.white)};
   text-transform: uppercase;
   text-decoration: none;
   outline: none;
@@ -32,10 +20,9 @@ const AppButton = AppText.withComponent("button").extend`
 
   ${props =>
     !props.empty &&
-    `
-    &:hover { ${AppButtonActive} }
-    ${props.active && AppButtonActive}
-  `}
+    `&:hover {
+      box-shadow: inset 0 0 999em rgba(0, 0, 0, 0.5);
+    }`}
 `;
 
 export default AppButton;
--- client/src/components/routes/login/Login.js
+++ client/src/components/routes/login/Login.js
@@ -1,5 +1,93 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import { Redirect } from "react-router-dom";
+
+import AppText from "components/common/AppText";
+import AppButton from "components/common/AppButton";
+import FullWidthContainer from "components/common/FullWidthContainer";
+
+const LoginMain = FullWidthContainer.withComponent("main").extend`
+  position: relative;
+  display: flex;
+  height: 100%;
+`;
+
+const LoginForm = styled.form`
+  position: relative;
+  z-index: 1;
+  margin: auto;
+`;
+
+const LoginLabel = styled.label`
+  display: block;
+  margin-bottom: 1rem;
+`;
+
+const LabelText = AppText.extend`
+  display: block;
+  margin-bottom: 0.4rem;
+  color: #404937;
+`;
+
+const LoginTextInput = AppText.withComponent("input").extend`
+  width: 18rem;
+  height: 2rem;
+  padding: 0.5rem;
+  border: none;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+`;
+
+const LoginButton = AppButton.withComponent("input").extend`
+  display: block;
+  width: 75%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  border-radius: 50%;
+`;
+
+const GreenCircle = Circle.extend`
+  top: -9vw;
+  left: -12vw;
+  width: 25vw;
+  max-width: 16rem;
+  height: 25vw;
+  max-height: 16rem;
+  background: #bfd8a6;
+`;
+
+const YellowCircle = Circle.extend`
+  top: -12vw;
+  right: 6vw;
+  width: 30vw;
+  max-width: 16rem;
+  height: 30vw;
+  max-height: 16rem;
+  background: #fadc84;
+`;
+
+const BlueCircle = Circle.extend`
+  bottom: -10vw;
+  left: 6vw;
+  width: 25vw;
+  max-width: 14rem;
+  height: 25vw;
+  max-height: 14rem;
+  background: #88cfc9;
+`;
+
+const FleshCircle = Circle.extend`
+  bottom: -12vw;
+  right: -5vw;
+  width: 35vw;
+  max-width: 20rem;
+  height: 35vw;
+  max-height: 20rem;
+  background: #ffe5cb;
+`;
 
 class Login extends Component {
   constructor(props) {
@@ -23,8 +111,8 @@ class Login extends Component {
 
     if (!props.isAuthenticated) {
       return (
-        <main>
-          <form
+        <LoginMain>
+          <LoginForm
             onSubmit={event =>
               this.props.handleLogIn(
                 event,
@@ -33,25 +121,29 @@ class Login extends Component {
               )
             }
           >
-            <label>
-              Email address:
-              <input
+            <LoginLabel>
+              <LabelText>Email address:</LabelText>
+              <LoginTextInput
                 type="email"
                 onChange={event => handleInputChange("email", event)}
               />
-            </label>
-            <label>
-              Password:
-              <input
+            </LoginLabel>
+            <LoginLabel style={{ marginBottom: "2rem" }}>
+              <LabelText>Password:</LabelText>
+              <LoginTextInput
                 type="password"
                 onChange={event => handleInputChange("password", event)}
               />
-            </label>
+            </LoginLabel>
             {/* <label><input type="checkbox" /> Keep me logged in</label> */}
             {/* <a href="#">Forgot password?</a> */}
-            <input type="submit" />
-          </form>
-        </main>
+            <LoginButton type="submit" value="Sign in" />
+          </LoginForm>
+          <GreenCircle />
+          <YellowCircle />
+          <BlueCircle />
+          <FleshCircle />
+        </LoginMain>
       );
     } else {
       return (
