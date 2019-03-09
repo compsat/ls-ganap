721646d839a0f3f7638550a3f92e02e9b0106af7
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-03-09 23:42:58 +0800

    add to wsgi

--- api/ls_ganap/wsgi.py
+++ api/ls_ganap/wsgi.py
@@ -1,8 +1,7 @@
+
 """
 WSGI config for ls_ganap project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
