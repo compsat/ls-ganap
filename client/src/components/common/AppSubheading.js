b8e1f67356d7c8e00c40fa2fc5d759dcc4dabc78
Eddrich Janzzen Ang <eddrichang@gmail.com>
2019-03-09 23:12:02 +0800

    remove client

--- client/src/components/common/AppSubheading.js
+++
@@ -1,9 +0,0 @@
-import AppText from "components/common/AppText";
-
-const AppSubheading = AppText.extend`
-  font-family: "Quatro Sans";
-  font-weight: 600;
-  ${props => props.caps && "text-transform: uppercase;"}
-`;
-
-export default AppSubheading;
--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import entities from "reducers/entities";
-import filters from "reducers/filters";
-
-export default combineReducers({
-  entities,
-  filters
-});
