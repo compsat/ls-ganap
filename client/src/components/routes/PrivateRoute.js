436406517996edc2990adf0b56573a448ac49136
Miguel N. Galace <galacemiguel@gmail.com>
2019-03-23 21:24:31 +0800

    Implement create event page

--- client/src/components/routes/PrivateRoute.js
+++ client/src/components/routes/PrivateRoute.js
@@ -1,21 +1,24 @@
-import { Route } from "react-router-dom";
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { referrer: props.location }
-          }}
-        />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { referrer: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
