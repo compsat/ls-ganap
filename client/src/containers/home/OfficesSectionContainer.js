3dadee0b6aed9fe60123b6d9304c2be7f1384267
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-24 17:01:44 +0800

    Add links for Office to browse

--- client/src/containers/home/OfficesSectionContainer.js
+++ client/src/containers/home/OfficesSectionContainer.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 
 import { fetchOffices } from "actions/offices";
+import { selectHost } from "actions/filters";
 import OfficesSection from "components/routes/home/OfficesSection";
 
 const mapStateToProps = state => ({
@@ -12,7 +13,8 @@ const mapDispatchToProps = dispatch => ({
     for (let i = 1; i < 4; i++) {
       dispatch(fetchOffices(i));
     }
-  }
+  },
+  selectHost: (host) => dispatch(selectHost(host))
 });
 
 export default connect(
