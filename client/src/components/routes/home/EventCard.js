62dc460953b4eb79c3f39bf4840cb038b7fd1ede
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-24 17:02:03 +0800

    Add Linking to image of EventCard

--- client/src/components/routes/home/EventCard.js
+++ client/src/components/routes/home/EventCard.js
@@ -5,8 +5,20 @@ import AppCardImage from "components/common/AppCardImage";
 import AppCardTextBox from "components/common/AppCardTextBox";
 import AppText from "components/common/AppText";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const AppTextP = AppText.withComponent("p");
+
+const AppCardLink = ({ className, route, children, onClick }) => (
+  <Link className={className} to={route} onClick={onClick}>
+    {children}
+  </Link>
+);
+
+const StyledAppCardLink = styled(AppCardLink)`
+  text-decoration: none;
+  color: black;
+`;
 
 const EventCard = ({
   eventId,
@@ -17,16 +29,18 @@ const EventCard = ({
   venue,
   poster_url
 }) => (
-  <AppCardBase>
-    <Link to={`/events/${eventId}`}><AppCardImage src={poster_url} aspectRatio={5 / 4} alt={name} /></Link>
-    <AppCardTextBox lines={3}>
-      <AppTextP>{name}</AppTextP>
-      {/* <AppTextP>{formattedHosts}</AppTextP> */}
-      <AppTextP>{formattedDate}</AppTextP>
-      {/* <AppTextP>{formattedTime}</AppTextP>
-      <AppTextP>{venue}</AppTextP> */}
-    </AppCardTextBox>
-  </AppCardBase>
+  <StyledAppCardLink route={`/events/${eventId}`}>
+    <AppCardBase>
+      <AppCardImage src={poster_url} aspectRatio={5 / 4} alt={name} />
+      <AppCardTextBox lines={3}>
+        <AppTextP>{name}</AppTextP>
+        {/* <AppTextP>{formattedHosts}</AppTextP> */}
+        <AppTextP>{formattedDate}</AppTextP>
+        {/* <AppTextP>{formattedTime}</AppTextP>
+        <AppTextP>{venue}</AppTextP> */}
+      </AppCardTextBox>
+    </AppCardBase>
+  </StyledAppCardLink>
 );
 
 export default EventCard;
