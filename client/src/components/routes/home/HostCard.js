97f653f8b2feada9dc3db54c75d428cce574a9b2
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-28 16:01:29 +0800

    Add selectHost to new container

--- client/src/components/routes/home/HostCard.js
+++ client/src/components/routes/home/HostCard.js
@@ -5,13 +5,29 @@ import AppCardImage from "components/common/AppCardImage";
 import AppCardTextBox from "components/common/AppCardTextBox";
 import AppText from "components/common/AppText";
 
-const HostCard = ({ id, name, logoUrl }) => (
-  <AppCardBase key={id}>
-    <AppCardImage src={logoUrl} size={"75%"} aspectRatio={1} alt={name} />
-    <AppCardTextBox lines={3} center={true}>
-      <AppText>{name}</AppText>
-    </AppCardTextBox>
-  </AppCardBase>
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const AppCardLink = ({ className, route, children, onClick }) => (
+  <Link className={className} to={route} onClick={onClick}>
+    {children}
+  </Link>
+);
+
+const StyledAppCardLink = styled(AppCardLink)`
+  text-decoration: none;
+  color: black;
+`;
+
+const HostCard = ({ host, id, name, logoUrl, selectHost }) => (
+	<StyledAppCardLink route="/browse" onClick={() => selectHost(host)}>
+	  <AppCardBase key={id}>
+	    <AppCardImage src={logoUrl} size={"75%"} aspectRatio={1} alt={name} />
+	    <AppCardTextBox lines={3} center={true}>
+	      <AppText>{name}</AppText>
+	    </AppCardTextBox>
+	  </AppCardBase>
+	</StyledAppCardLink>
 );
 
 export default HostCard;
