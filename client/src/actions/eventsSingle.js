8c46089ded304b09486d176e5b74614b38e85c6b
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-28 13:41:24 +0800

    Add eventsSingle action and reducer

---
+++ client/src/actions/eventsSingle.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { normalize } from "normalizr";
+
+import { addEntityEvents } from "actions/entities";
+import { fetchHosts } from "actions/hosts";
+import { fetchVenues } from "actions/venues";
+import event from "entities/events";
+
+export const FETCH_EVENTS_SINGLE_REQUEST = "FETCH_EVENTS_SINGLE_REQUEST";
+export const fetchEventsSingleRequest = () => ({
+  type: FETCH_EVENTS_SINGLE_REQUEST
+});
+
+export const FETCH_EVENTS_SINGLE_SUCCESS = "FETCH_EVENTS_SINGLE_SUCCESS";
+export const fetchEventsSingleSuccess = event => ({
+  type: FETCH_EVENTS_SINGLE_SUCCESS,
+  event
+});
+
+export const FETCH_EVENTS_SINGLE_FAILURE = "FETCH_EVENTS_SINGLE_FAILURE";
+export const fetchEventsSingleFailure = () => ({
+  type: FETCH_EVENTS_SINGLE_FAILURE
+});
+
+export const fetchEventsSingle = (pk) => {
+  return dispatch => {
+    dispatch(fetchHosts());
+    dispatch(fetchVenues());
+    dispatch(fetchEventsSingleRequest());
+
+    return axios
+      .get(`/events/${pk}/`)
+      .then(response => {
+        const payload = response.data;
+        const normalizedData = normalize({payload}, [event]);
+        dispatch(addEntityEvents(normalizedData.entities.events));
+        dispatch(fetchEventsSingleSuccess(normalizedData.result));
+      })
+      .catch(error => {
+        dispatch(fetchEventsSingleFailure());
+      });
+  };
+};
