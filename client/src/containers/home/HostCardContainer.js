d499c9f8a464c713937ded0de73a30a54c695a78
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-28 16:01:29 +0800

    Add selectHost to new container

--- client/src/containers/home/HostCardContainer.js
+++ client/src/containers/home/HostCardContainer.js
@@ -2,16 +2,25 @@ import { connect } from "react-redux";
 
 import HostCard from "components/routes/home/HostCard";
 import { makeDenormalizeHost } from "selectors/hostsSelectors";
+import { selectHost } from "actions/filters";
 
 const mapStateToProps = (state, ownProps) => {
   const denormalizeHost = makeDenormalizeHost(ownProps.hostType, ownProps.id);
   const host = denormalizeHost(state, ownProps);
 
   return {
+  	host: host,
     id: host.id,
     name: host.name,
     logoUrl: host.logo_url
   };
 };
 
-export default connect(mapStateToProps)(HostCard);
+const mapDispatchToProps = dispatch => ({
+  selectHost: (host) => dispatch(selectHost(host))
+});
+
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(HostCard);
