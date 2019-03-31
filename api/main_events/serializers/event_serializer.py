06eb49ad7e00f0be3112b73ab828ab6186e02bbc
Daniel <daniel.delrio@obf.ateneo.edu>
2019-03-31 14:46:23 +0800

    Add hosts field to event_serializer when creating an event

--- api/main_events/serializers/event_serializer.py
+++ api/main_events/serializers/event_serializer.py
@@ -39,11 +39,64 @@ class EventSerializer(serializers.ModelSerializer):
 
 class CreateEventSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True)
+    hosts = serializers.ListField(
+       child=serializers.IntegerField(),
+       required=False, write_only=True
+    )
+
+    def validate_hosts(self, hosts):
+        for host in hosts:
+            if not OrgHost.objects.filter(pk=host).exists() and not SangguHost.objects.filter(pk=host).exists() and not OfficeHost.objects.filter(pk=host).exists():
+                raise serializers.ValidationError("Host with index {} does not exist".format(host))
+
+        return hosts
+
+    def create(self, validated_data):
+        hosts_data = None
+
+        if 'hosts' in validated_data:
+            hosts_data = validated_data.pop('hosts', None)
+
+        tags_data = validated_data.pop('tags', None)
+        event = Event.objects.create(**validated_data)
+        creator = event.created_by
+
+        if tags_data:
+            event.tags.set(tags_data)
+
+        if hosts_data:
+            for host in hosts_data:
+                if OrgHost.objects.filter(pk=host).exists():
+                    event.org_hosts.add(OrgHost.objects.get(pk=host))
+                elif SangguHost.objects.filter(pk=host).exists():
+                    event.sanggu_hosts.add(SangguHost.objects.get(pk=host))
+                elif OfficeHost.objects.filter(pk=host).exists():
+                    event.office_hosts.add(OfficeHost.objects.get(pk=host))
+
+        # automatically adds creator to list of hosts if it's not part
+        if OrgHost.objects.filter(pk=creator).exists():
+            org_host = OrgHost.objects.get(pk=creator)
+            if org_host not in event.org_hosts.all():
+                event.org_hosts.add(org_host)
+
+        elif SangguHost.objects.filter(pk=creator).exists():
+            sanggu_host = SangguHost.objects.get(pk=creator)
+            if sanggu_host not in event.sanggu_hosts.all():
+                event.sanggu_hosts.add(sanggu_host)
+                
+        elif OfficeHost.objects.filter(pk=creator).exists():
+            office_host = OfficeHost.objects.get(pk=creator)
+            if office_host not in event.office_hosts.all():
+                event.office_hosts.add(office_host)
+
+        event.save()
+        return event
 
     class Meta:
         model = Event
         fields = ['id', 
                 'name',
+                'hosts',
                 'sanggu_hosts',
                 'office_hosts',
                 'org_hosts',
@@ -51,3 +104,4 @@ class CreateEventSerializer(serializers.ModelSerializer):
                 'poster_url', 
                 'event_url',
                 'tags']
+                
