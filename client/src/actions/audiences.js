7e99a99fb7a925e8723d142ce17d2bbae96d589b
Daniel <daniel.delrio@obf.ateneo.edu>
2019-08-25 10:17:00 +0800

    Remove console logs

--- client/src/actions/audiences.js
+++ client/src/actions/audiences.js
@@ -37,7 +37,6 @@ export const fetchAudiences = () => {
           dispatch(fetchAudienceSuccess(payload));
         })
         .catch(error => {
-          console.log(error);
           dispatch(fetchAudienceFailure());
         });
     }
