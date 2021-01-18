73b0156c94bce8089969b387c651efcfb603a302
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-01-18 16:06:56 +0800

    Remove sentry from local settings

--- api/ls_ganap/dev_settings.py
+++ api/ls_ganap/dev_settings.py
@@ -8,16 +8,3 @@ ALLOWED_HOSTS = [
 ]
 
 DEFAULT_DOMAIN = 'http://localhost:3000'
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://e44bd6b8118a4cd5ab4b0fc9864d3b05@o505407.ingest.sentry.io/5593644",
-    integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
